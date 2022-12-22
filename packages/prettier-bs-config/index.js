f504eed532e02760d718f72bf2de623076da115f
Ashu <ashutosh.d@browserstack.com>
2022-12-22 23:10:43 +0530

    finalizing prettier configuration

---
+++ packages/prettier-bs-config/index.js
@@ -0,0 +1,9 @@
+/**
+ * if you wish to extend this config,
+ * add a .prettiecrc.js to your project and
+ * require() this package in that file, then
+ * remove the 'prettier' property from package.json
+ * https://prettier.io/docs/en/configuration.html#sharing-configurations
+ */
+
+module.exports = { printWidth: 120, singleQuote: true, arrowParens: "always" };
