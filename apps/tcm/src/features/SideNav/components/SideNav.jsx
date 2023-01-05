ba27b1712e68a2a8dae5ad25f80bb04e6af6faef
ribinBS <ribin@browserstack.com>
2023-01-05 20:44:53 +0530

    sidebar ini progress

--- apps/tcm/src/features/SideNav/components/SideNav.jsx
+++ apps/tcm/src/features/SideNav/components/SideNav.jsx
@@ -1,18 +1,60 @@
+import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { SidebarNavigation } from '@browserstack/bifrost';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import AppRoute from 'const/routes';
 
 const SideNav = () => {
-  const primaryNavs = [
+  const location = useLocation();
+  const [primaryNavs, setPrimaryNavs] = useState([
     {
-      id: 'dashboard',
-      label: 'Dashboard',
-      // activeIcon: HomeIcon,
-      // inActiveIcon: HomeIcon,
-      path: '/',
-      badgeLabel: 'Active'
+      id: AppRoute.PROJECTS,
+      label: 'All Projects',
+      activeIcon: HomeOutlinedIcon,
+      inActiveIcon: HomeOutlinedIcon,
+      path: AppRoute.PROJECTS
+    },
+    {
+      id: 'settings',
+      label: 'Settings',
+      activeIcon: HomeOutlinedIcon,
+      inActiveIcon: HomeOutlinedIcon,
+      path: AppRoute.TEST_RUNS
+    },
+    {
+      id: 'documentation',
+      label: 'Documentation',
+      activeIcon: HomeOutlinedIcon,
+      inActiveIcon: HomeOutlinedIcon
     }
+  ]);
+  const [secondaryNavs, setSecondaryNavs] = useState([]);
+  const [showProjects, setShowProjects] = useState(false);
+
+  const selectOptions = [
+    { label: 'Project 1', value: 'p1' },
+    { label: 'Project 2', value: 'p2' },
+    { label: 'Project 3', value: 'p3' }
   ];
 
-  return <SidebarNavigation wrapperClass="mt-16" />;
+  const onLinkChange = (a, b, c) => {
+    debugger;
+  };
+
+  useEffect(() => {}, [location.pathname]);
+
+  console.log(location.pathname);
+  return (
+    <SidebarNavigation
+      wrapperClass="mt-16"
+      primaryNavItems={primaryNavs}
+      active={primaryNavs[0]}
+      handleClick={onLinkChange}
+      selectOptions={selectOptions}
+      withSelect={showProjects}
+      secondaryNavItems={secondaryNavs}
+    />
+  );
 };
 
 export default SideNav;
