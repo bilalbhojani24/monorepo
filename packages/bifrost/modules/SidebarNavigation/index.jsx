a333171c2255d8a8e8c10262aade0d2739dc1b29
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-01-05 19:26:51 +0530

    vscode moved to gitignore and sidebar width changed

--- packages/bifrost/modules/SidebarNavigation/index.jsx
+++ packages/bifrost/modules/SidebarNavigation/index.jsx
@@ -1,10 +1,12 @@
-import React, { useState, Fragment } from 'react';
-import PropTypes from 'prop-types';
+import React, { Fragment, useState } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+import { Bars3Icon, XMarkIcon } from '../Icon';
 import SelectMenu from '../SelectMenu';
+
 import SidebarItem from './component/SidebarItem';
-import { XMarkIcon, Bars3Icon } from '../Icon';
 
 import './styles.scss';
 
@@ -36,7 +38,11 @@ const SidebarNavigation = (props) => {
   return (
     <>
       <Transition.Root show={sidebarOpen} as={Fragment}>
-        <Dialog as="div" className="relative z-40 md:hidden" onClose={setSidebarOpen}>
+        <Dialog
+          as="div"
+          className="relative z-40 md:hidden"
+          onClose={setSidebarOpen}
+        >
           <Transition.Child
             as={Fragment}
             enter="transition-opacity ease-linear duration-300"
@@ -76,14 +82,20 @@ const SidebarNavigation = (props) => {
                       onClick={() => setSidebarOpen(false)}
                     >
                       <span className="sr-only">Close sidebar</span>
-                      <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                      <XMarkIcon
+                        className="h-6 w-6 text-white"
+                        aria-hidden="true"
+                      />
                     </button>
                   </div>
                 </Transition.Child>
                 <div className="h-0 flex-1 overflow-y-auto pt-5 pb-4">
-                  {!!brandImage.length ? (
+                  {brandImage.length ? (
                     <div
-                      className={classNames('flex flex-shrink-0 items-center space-y-5 px-4', brandImageContainerClass)}
+                      className={classNames(
+                        'flex flex-shrink-0 items-center space-y-5 px-4',
+                        brandImageContainerClass,
+                      )}
                     >
                       <img
                         className={classNames('h-8 w-auto', brandImageClass)}
@@ -108,7 +120,7 @@ const SidebarNavigation = (props) => {
                     ))}
                   </nav>
                 </div>
-                <div className="flex flex-col flex-shrink-0 px-1">
+                <div className="flex shrink-0 flex-col px-1">
                   {secondaryNavItems.map((nav, index) => (
                     <SidebarItem
                       active={active}
@@ -124,25 +136,44 @@ const SidebarNavigation = (props) => {
                 </div>
               </Dialog.Panel>
             </Transition.Child>
-            <div className="w-14 flex-shrink-0">{/* Force sidebar to shrink to fit close icon */}</div>
+            <div className="w-14 shrink-0">
+              {/* Force sidebar to shrink to fit close icon */}
+            </div>
           </div>
         </Dialog>
       </Transition.Root>
 
       {/* Static sidebar for desktop */}
-      <div className={classNames('hidden md:fixed md:inset-y-0 md:flex md:w-72 md:flex-col py-2 ', wrapperClass)}>
+      <div
+        className={classNames(
+          'hidden py-2 md:fixed md:inset-y-0 md:flex md:w-64 md:flex-col ',
+          wrapperClass,
+        )}
+      >
         <div className="flex min-h-0 flex-1 flex-col border-r border-gray-200 bg-white">
           <div className="flex flex-1 flex-col overflow-y-auto pb-2">
-            {!!brandImage.length ? (
-              <div className={classNames('flex flex-shrink-0 items-center space-y-2 px-4', brandImageContainerClass)}>
-                <img className={classNames('h-8 w-auto', brandImageClass)} src={brandImage} alt="sidebar-nav-icon" />
+            {brandImage.length ? (
+              <div
+                className={classNames(
+                  'flex flex-shrink-0 items-center space-y-2 px-4',
+                  brandImageContainerClass,
+                )}
+              >
+                <img
+                  className={classNames('h-8 w-auto', brandImageClass)}
+                  src={brandImage}
+                  alt="sidebar-nav-icon"
+                />
               </div>
             ) : null}
 
             <nav className="mt-5 flex-1 space-y-2 bg-white px-2">
               {withSelect ? (
                 <div className="mb-5">
-                  <SelectMenu options={selectOptions} value={selectOptions[0]} />
+                  <SelectMenu
+                    options={selectOptions}
+                    value={selectOptions[0]}
+                  />
                 </div>
               ) : null}
 
@@ -160,7 +191,7 @@ const SidebarNavigation = (props) => {
               ))}
             </nav>
           </div>
-          <div className="flex flex-col flex-shrink-0 px-1">
+          <div className="flex shrink-0 flex-col px-1">
             {secondaryNavItems.map((nav, index) => (
               <SidebarItem
                 active={active}
@@ -214,9 +245,9 @@ SidebarNavigation.propTypes = {
           id: PropTypes.string,
           label: PropTypes.string,
           path: PropTypes.string,
-        })
+        }),
       ),
-    })
+    }),
   ),
   secondaryNavItems: PropTypes.arrayOf(
     PropTypes.shape({
@@ -231,16 +262,16 @@ SidebarNavigation.propTypes = {
           id: PropTypes.string,
           label: PropTypes.string,
           path: PropTypes.string,
-        })
+        }),
       ),
-    })
+    }),
   ),
   selectOptions: PropTypes.arrayOf(
     PropTypes.shape({
       value: PropTypes.number,
       label: PropTypes.string,
       image: PropTypes.string,
-    })
+    }),
   ),
   withSelect: PropTypes.bool,
   wrapperClass: PropTypes.string,
@@ -253,6 +284,7 @@ SidebarNavigation.defaultProps = {
   brandImageClass: '',
   handleClick: () => {},
   inActiveClass: '',
+  inActiveIconClass: '',
   primaryNavItems: [],
   secondaryNavItems: [],
   selectOptions: [],
