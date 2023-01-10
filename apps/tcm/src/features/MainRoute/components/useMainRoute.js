6a77e793a59abe5d51854984d1fe099058c9c5a0
ribinBS <ribin@browserstack.com>
2023-01-10 14:46:18 +0530

    response structure updaate

--- apps/tcm/src/features/MainRoute/components/useMainRoute.js
+++ apps/tcm/src/features/MainRoute/components/useMainRoute.js
@@ -11,7 +11,6 @@ export default function useMainRoute() {
   const cookies = new Cookies();
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  window.TCM = { logged_in: true };
 
   useEffect(() => {
     if (window.location.host === 'localhost:5173') {
@@ -29,18 +28,16 @@ export default function useMainRoute() {
       }
     } else {
       authUser()
-        .then((data) => {
-          if (data.response.data?.data?.logged_in) {
-            cookies.set(AUTH_TOKEN_KEY, 'true');
-            navigate(AppRoute.PROJECTS);
-          }
-          if (data.response.data?.data?.user) {
-            dispatch(setUser(data.response.data.data.user));
+        .then((res) => {
+          cookies.set(AUTH_TOKEN_KEY, 'true');
+          navigate(AppRoute.PROJECTS);
+          if (res.data?.data?.user) {
+            dispatch(setUser(res.data.data.user));
           }
         })
-        .catch((data) => {
-          if (data.response?.data?.data?.login_url) {
-            dispatch(setLoginURL(data.response.data.data.login_url));
+        .catch((res) => {
+          if (res?.data?.data?.login_url) {
+            dispatch(setLoginURL(res.data.data.login_url));
             cookies.remove(AUTH_TOKEN_KEY);
             navigate(AppRoute.LANDING);
           }
