b30e5a3051912c56919fbf7c66e7ba8f853ae772
ribinBS <ribin@browserstack.com>
2023-01-10 15:01:24 +0530

    route redirect fix

--- apps/tcm/src/features/MainRoute/components/useMainRoute.js
+++ apps/tcm/src/features/MainRoute/components/useMainRoute.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { authUser } from 'api/auth.api';
 import { AUTH_TOKEN_KEY } from 'const/immutables';
 import AppRoute from 'const/routes';
@@ -10,18 +10,19 @@ import Cookies from 'universal-cookie';
 export default function useMainRoute() {
   const cookies = new Cookies();
   const navigate = useNavigate();
+  const location = useLocation();
   const dispatch = useDispatch();
 
   useEffect(() => {
     if (window.location.host === 'localhost:5173') {
       // mock for localhost
       if (localStorage.getItem('TCM_LOGGED_OUT') === 'true') {
-        dispatch(setLoginURL('https://browserstack.com/'));
         cookies.remove(AUTH_TOKEN_KEY);
+        dispatch(setLoginURL('https://browserstack.com/'));
         navigate(AppRoute.LANDING);
       } else {
         cookies.set(AUTH_TOKEN_KEY, 'true');
-        navigate(AppRoute.ROOT);
+        if (location.pathname === AppRoute.LANDING) navigate(AppRoute.ROOT);
         dispatch(
           setUser({ full_name: 'Faker Name', email: 'fake2@example.com' }),
         );
@@ -30,7 +31,7 @@ export default function useMainRoute() {
       authUser()
         .then((res) => {
           cookies.set(AUTH_TOKEN_KEY, 'true');
-          navigate(AppRoute.ROOT);
+          if (location.pathname === AppRoute.LANDING) navigate(AppRoute.ROOT);
           if (res.data?.data?.user) {
             dispatch(setUser(res.data.data.user));
           }
