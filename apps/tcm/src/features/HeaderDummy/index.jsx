a70a8aeb3a058393f54249c4b2250bd8190c598d
ribinBS <ribin@browserstack.com>
2023-01-10 14:36:11 +0530

    header update

--- apps/tcm/src/features/HeaderDummy/index.jsx
+++ apps/tcm/src/features/HeaderDummy/index.jsx
@@ -5,13 +5,11 @@ import { SectionHeadings } from '@browserstack/bifrost';
 const HeaderDummy = () => {
   const userDetails = useSelector((state) => state.global.user);
 
+  console.log('from header', userDetails);
+
   return (
     <div className="fixed top-0 z-[99] w-full bg-white pt-5">
-      <SectionHeadings
-        title={`BrowserStack | TM ${
-          userDetails?.full_name ? ` | ${userDetails?.full_name}` : ''
-        }`}
-      />
+      <SectionHeadings title="BrowserStack | TM" />
     </div>
   );
 };
