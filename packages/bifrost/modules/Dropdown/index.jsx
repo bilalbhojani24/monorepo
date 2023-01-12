af85b021877920b23800d49f3fc84670b2ffc7fa
samarthBrowserstack <samarth.s@browserstack.com>
2023-01-12 18:23:42 +0530

    Fix: colors on kpi and data viz

--- packages/bifrost/modules/Dropdown/index.jsx
+++ packages/bifrost/modules/Dropdown/index.jsx
@@ -2,12 +2,23 @@ import React, { Fragment } from 'react';
 import { Menu, Transition } from '@headlessui/react';
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
+
 import { ChevronDownIcon, EllipsisVerticalIcon } from '../Icon';
-import './styles.scss';
+
 import { DROPDOWN_TYPES } from './const/dropdownConstants';
 
+import './styles.scss';
+
 const Dropdown = (props) => {
-  const { options, dividerRequired, triggerTitle, triggerVariant, headerRequired, heading, subHeading } = props;
+  const {
+    options,
+    dividerRequired,
+    triggerTitle,
+    triggerVariant,
+    headerRequired,
+    heading,
+    subHeading,
+  } = props;
 
   return (
     <Menu as="div" className="relative inline-block text-left">
@@ -15,22 +26,26 @@ const Dropdown = (props) => {
         {triggerVariant === 'text-button' && (
           <Menu.Button className="inline-flex w-full justify-center rounded-md border border-base-300 bg-white px-4 py-2 text-sm font-medium text-base-700 shadow-sm hover:bg-base-50 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-base-100">
             {triggerTitle}
-            <ChevronDownIcon className="-mr-1 ml-2 h-5 w-5" aria-hidden="true" />
+            <ChevronDownIcon
+              className="-mr-1 ml-2 h-5 w-5"
+              aria-hidden="true"
+            />
           </Menu.Button>
         )}
         {['meatball-button', 'menu-button'].includes(triggerVariant) && (
           <Menu.Button
             className={classNames(
-              'flex items-center rounded-full bg-base-100 text-base-400 hover:text-base-600 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-base-100 p-2',
+              'flex items-center rounded-full bg-base-100 text-base-400 hover:text-base-600 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2 focus:ring-offset-base-100',
               {
-                'bg-white rounded-lg border border-gray border-2': triggerVariant === DROPDOWN_TYPES[2]
-              }
+                'border-gray rounded-lg border border-2 bg-white p-2':
+                  triggerVariant === DROPDOWN_TYPES[2],
+              },
             )}
           >
             <span className="sr-only">Open options</span>
             <EllipsisVerticalIcon
               className={classNames('h-5 w-5', {
-                'text-base-700': triggerVariant === DROPDOWN_TYPES[2]
+                'text-base-700': triggerVariant === DROPDOWN_TYPES[2],
               })}
               aria-hidden="true"
             />
@@ -47,11 +62,13 @@ const Dropdown = (props) => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-base-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+        <Menu.Items className="divide-base-100 absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
           {headerRequired && (
             <div className="px-4 py-3">
               <p className="text-sm">{heading}</p>
-              <p className="truncate text-sm font-medium text-base-900">{subHeading}</p>
+              <p className="text-base-900 truncate text-sm font-medium">
+                {subHeading}
+              </p>
             </div>
           )}
           <div className="py-1">
@@ -64,9 +81,10 @@ const Dropdown = (props) => {
                       {
                         'bg-base-100 text-base-900': active,
                         'text-base-700': !active,
-                        'border-b border-base-100': dividerRequired && optionIdx !== options.length - 1
+                        'border-b border-base-100':
+                          dividerRequired && optionIdx !== options.length - 1,
                       },
-                      'block px-4 py-2 text-sm'
+                      'block px-4 py-2 text-sm',
                     )}
                   >
                     {option.name}
@@ -87,14 +105,14 @@ Dropdown.propTypes = {
     PropTypes.shape({
       id: PropTypes.string,
       name: PropTypes.string,
-      url: PropTypes.string
-    })
+      url: PropTypes.string,
+    }),
   ).isRequired,
   dividerRequired: PropTypes.bool,
   triggerVariant: PropTypes.oneOf(DROPDOWN_TYPES),
   headerRequired: PropTypes.bool,
   heading: PropTypes.string,
-  subHeading: PropTypes.string
+  subHeading: PropTypes.string,
 };
 Dropdown.defaultProps = {
   triggerTitle: 'Options',
@@ -105,13 +123,13 @@ Dropdown.defaultProps = {
     { id: '4', name: 'Move', url: 'www.google.com' },
     { id: '5', name: 'Share', url: 'www.google.com' },
     { id: '6', name: 'Add to favorites', url: 'www.google.com' },
-    { id: '7', name: 'Delete', url: 'www.google.com' }
+    { id: '7', name: 'Delete', url: 'www.google.com' },
   ],
   divider: false,
   triggerVariant: DROPDOWN_TYPES[0],
   headerRequired: false,
   heading: 'Signed in as',
-  subHeading: 'tom@example.com'
+  subHeading: 'tom@example.com',
 };
 
 export default Dropdown;
