52f93f252059fc1b4bf969dcf3089feba66d05a7
ribinBS <ribin@browserstack.com>
2023-01-16 14:40:20 +0530

    removed await

--- apps/tcm/src/api/projects.api.js
+++ apps/tcm/src/api/projects.api.js
@@ -1,15 +1,15 @@
 import { fetchGet, fetchPost } from './_utils/fetch';
 
-export const getProjectsAPI = async () => await fetchGet('/api/v1/projects');
+export const getProjectsAPI = async () => fetchGet('/api/v1/projects');
 
 export const addProjectsAPI = async (payload) =>
   // const data = payload;
-  await fetchPost(`/api/v1/projects`, payload);
+  fetchPost(`/api/v1/projects`, payload);
 
 export const editProjectAPI = async (projectId, payload) =>
   // const data = payload;
-  await fetchPost(`/api/v1/projects/${projectId}/edit`, payload);
+  fetchPost(`/api/v1/projects/${projectId}/edit`, payload);
 
 export const deleteProjectAPI = async (projectId) =>
   // const data = payload;
-  await fetchPost(`/api/v1/projects/${projectId}/delete`);
+  fetchPost(`/api/v1/projects/${projectId}/delete`);
