a0944844528c041536c2701bc1a797666e297bca
ribinBS <ribin@browserstack.com>
2023-01-17 19:31:34 +0530

    header text update

--- apps/tcm/src/features/HeaderDummy/index.jsx
+++ apps/tcm/src/features/HeaderDummy/index.jsx
@@ -5,7 +5,7 @@ import AppRoute from 'const/routes';
 
 const HeaderDummy = () => (
   <div className="border-base-300 fixed top-0 z-[99] h-16 w-full border-b bg-white pt-5">
-    <Link to={AppRoute.ROOT}>BrowserStack | Test Case Management</Link>
+    <Link to={AppRoute.ROOT}>BrowserStack | Test Management</Link>
   </div>
 );
 
