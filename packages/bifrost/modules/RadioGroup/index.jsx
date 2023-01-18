2e163f4b5965648f03bc20248ff326a3aded39a0
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-01-18 18:36:03 +0530

    radio and checkbox controlled and uncontrolled changes and defaultSelected for radio grooup issue resolved

--- packages/bifrost/modules/RadioGroup/index.jsx
+++ packages/bifrost/modules/RadioGroup/index.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import './styles.scss';
+import PropTypes from 'prop-types';
+
 import { DIRECTIONS } from './const/radioGroupConstants';
 
+import './styles.scss';
+
 const RadioGroup = (props) => {
-  const { direction, options, onChange, inlineDescription, rightAligned } = props;
-  const [selectedId, setSelectedId] = useState(options[0]?.id);
+  const {
+    direction,
+    defaultSelectedOption,
+    options,
+    onChange,
+    inlineDescription,
+    rightAligned,
+  } = props;
+  const [selectedId, setSelectedId] = useState(defaultSelectedOption?.id);
 
   const handleChange = (id) => (e) => {
     setSelectedId(id);
@@ -17,32 +26,44 @@ const RadioGroup = (props) => {
     <div
       className={classNames('flex', {
         'space-x-5': direction === DIRECTIONS[0],
-        'space-y-5 flex-col': direction === DIRECTIONS[1]
+        'space-y-5 flex-col': direction === DIRECTIONS[1],
       })}
     >
       {options.map((option) => (
         <div key={option.id} className="flex items-start">
-          <div className={classNames('flex h-5 items-center', { 'order-last mx-3': rightAligned })}>
+          <div
+            className={classNames('flex h-5 items-center', {
+              'order-last mx-3': rightAligned,
+            })}
+          >
             <input
               id={option.id}
               aria-describedby={`${option.id}-description`}
               name="plan"
               type="radio"
-              defaultChecked={option.id === selectedId}
-              className="h-4 w-4 border-base-300 text-brand-600 focus:ring-brand-500"
+              checked={option.id === selectedId}
+              className="border-base-300 text-brand-600 focus:ring-brand-500 h-4 w-4"
               onChange={handleChange(option.id)}
             />
           </div>
-          <div className={classNames({ 'flex-col': !inlineDescription, 'flex-1': rightAligned }, 'flex ml-3 text-sm')}>
+          <div
+            className={classNames(
+              { 'flex-col': !inlineDescription, 'flex-1': rightAligned },
+              'flex ml-3 text-sm',
+            )}
+          >
             {option.name && (
-              <label htmlFor={option.id} className="font-medium text-base-700">
+              <label htmlFor={option.id} className="text-base-700 font-medium">
                 {option.name}
               </label>
             )}
             {option.description && (
               <div
                 id={`${option.id}-description`}
-                className={classNames({ 'ml-2': inlineDescription }, 'text-base-500')}
+                className={classNames(
+                  { 'ml-2': inlineDescription },
+                  'text-base-500',
+                )}
               >
                 {option.description}
               </div>
@@ -56,43 +77,31 @@ const RadioGroup = (props) => {
 
 RadioGroup.propTypes = {
   direction: PropTypes.oneOf(DIRECTIONS),
+  defaultSelectedOption: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    name: PropTypes.string,
+    description: PropTypes.string,
+    disabled: PropTypes.bool,
+  }),
+  inlineDescription: PropTypes.bool,
   options: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string,
       description: PropTypes.string,
-      disabled: PropTypes.bool
-    })
-  ).isRequired,
+      disabled: PropTypes.bool,
+    }),
+  ),
   onChange: PropTypes.func,
-  inlineDescription: PropTypes.bool,
-  rightAligned: PropTypes.bool
+  rightAligned: PropTypes.bool,
 };
 RadioGroup.defaultProps = {
   direction: DIRECTIONS[0],
-  options: [
-    {
-      id: 'radio-1 ',
-      name: 'Radio 1 and it can grow longer and much more longer',
-      description: 'It is the description of Radio 1',
-      disabled: false
-    },
-    {
-      id: 'radio-2',
-      name: 'Radio 2 and it can grow longer',
-      description: 'It is the description of Radio 2',
-      disabled: false
-    },
-    {
-      id: 'radio-3',
-      name: 'Radio 3 and it can grow longer',
-      description: 'It is the description of Radio 3',
-      disabled: false
-    }
-  ],
-  onChange: () => {},
+  defaultSelectedOption: null,
   inlineDescription: false,
-  rightAligned: false
+  options: [],
+  onChange: () => {},
+  rightAligned: false,
 };
 
 export default RadioGroup;
