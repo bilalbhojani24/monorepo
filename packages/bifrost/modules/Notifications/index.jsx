37998b11d976b71e54be752ec42a85ad631f856b
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-01-23 17:49:05 +0530

    notifications, button changes, slideroverlay issue, popover and tooltip scss eliminated

--- packages/bifrost/modules/Notifications/index.jsx
+++ packages/bifrost/modules/Notifications/index.jsx
@@ -1,131 +1,112 @@
-import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
-import { Transition } from '@headlessui/react';
-import { CheckCircleIcon } from '@heroicons/react/24/outline';
-import Button from '../Button';
-import { XMarkIcon } from '../Icon';
+import React, { useContext } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+import { NotificationsContextData } from '../../shared/notificationsContext';
+import { XMarkIcon } from '../Icon';
 
 import './styles.scss';
 
+export const NotificationsContainer = () => (
+  <Toaster
+    position="top-right"
+    reverseOrder={false}
+    gutter={8}
+    containerClassName=""
+    containerStyle={{}}
+    toastOptions={{}}
+  />
+);
+
+export const notify = (Layout, options) =>
+  toast.custom(
+    (toastData) => (
+      <NotificationsContextData.Provider
+        value={{
+          toast: toastData
+        }}
+      >
+        <div
+          className={`w-full max-w-sm ${
+            toastData.visible ? 'animate-enter' : 'animate-leave'
+          } `}
+        >
+          {Layout}
+        </div>
+      </NotificationsContextData.Provider>
+    ),
+    options
+  );
+
+notify.remove = toast.remove;
+notify.dismiss = toast.dismiss;
+
 const Notifications = (props) => {
   const {
+    actionButtons,
     description,
     isCondensed,
     handleClose,
-    handlePositiveClick,
-    negativeButtonProps,
-    NotificationIcon,
-    NotificationIconClassName,
-    positiveButtonProps,
-    show,
+    headerIcon,
     title
   } = props;
 
-  const renderButton = (props) => {
-    if (Button) return <Button {...props} />;
-  };
+  const toastCtx = useContext(NotificationsContextData);
 
   return (
-    <div
-      aria-live="assertive"
-      className="pointer-events-none fixed inset-0 flex items-end px-4 py-6 sm:items-start sm:p-6"
-    >
-      <div className="flex w-full flex-col items-center space-y-4 sm:items-end">
-        <Transition
-          show={show}
-          as={Fragment}
-          enter="transform ease-out duration-300 transition"
-          enterFrom="translate-y-2 opacity-0 sm:translate-y-0 sm:translate-x-2"
-          enterTo="translate-y-0 opacity-100 sm:translate-x-0"
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <div className="pointer-events-auto w-full max-w-sm overflow-hidden rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5">
-            <div className="p-4">
-              <div className="flex items-start">
-                <div className="flex-shrink-0">
-                  {!isCondensed && (
-                    <NotificationIcon
-                      className={`h-6 w-6 text-base-400 ${NotificationIconClassName}`}
-                      aria-hidden="true"
-                    />
-                  )}
-                </div>
-                <div
-                  className={classNames('w-0 flex-1 pt-0.5', {
-                    'ml-3': !isCondensed
-                  })}
-                >
-                  <p className="text-sm font-medium text-base-900">{title}</p>
-                  {!isCondensed && <p className="mt-1 text-sm text-base-500">{description}</p>}
-                  {!isCondensed && (
-                    <div
-                      className={classNames('mt-3 flex space-x-7', {
-                        hidden: !negativeButtonProps?.children && !positiveButtonProps?.children
-                      })}
-                    >
-                      {renderButton(positiveButtonProps)}
-                      {renderButton(negativeButtonProps)}
-                    </div>
-                  )}
-                </div>
-                <div className="ml-4 flex flex-shrink-0">
-                  {isCondensed && (
-                    <button
-                      type="button"
-                      className="mr-3 flex-shrink-0 rounded-md bg-white text-sm font-medium text-brand-600 hover:text-brand-500 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2"
-                      onClick={() => {
-                        if (handlePositiveClick) handlePositiveClick();
-                      }}
-                    >
-                      Undo
-                    </button>
-                  )}
-                  <button
-                    type="button"
-                    className="inline-flex rounded-md bg-white text-base-400 hover:text-base-500 focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2"
-                    onClick={() => {
-                      if (handleClose) handleClose();
-                    }}
-                  >
-                    <span className="sr-only">Close</span>
-                    <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-                  </button>
-                </div>
-              </div>
-            </div>
+    <div className="pointer-events-auto flex w-full max-w-sm items-start rounded-lg bg-white p-4 shadow-lg ring-1 ring-black/5">
+      <div className="shrink-0 ">
+        {!isCondensed && headerIcon && headerIcon}
+      </div>
+      <div
+        className={classNames('w-0 flex-1 pt-0.5', {
+          'ml-3': !isCondensed
+        })}
+      >
+        <p className="text-base-900 text-sm font-medium">{title}</p>
+        {!isCondensed && (
+          <p className="text-base-500 mt-1 text-sm">{description}</p>
+        )}
+        {!isCondensed && (
+          <div className={classNames('mt-3 flex space-x-4')}>
+            {actionButtons?.(toastCtx?.toast || null)}
           </div>
-        </Transition>
+        )}
+      </div>
+      <div className="flex shrink-0">
+        {isCondensed && actionButtons?.(toastCtx?.toast || null)}
+        <button
+          type="button"
+          className="text-base-400 hover:text-base-500 focus:ring-brand-500 ml-4 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+          onClick={() => {
+            if (handleClose) handleClose(toastCtx?.toast || null);
+          }}
+        >
+          <span className="sr-only">Close</span>
+          <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+        </button>
       </div>
     </div>
   );
 };
 
 Notifications.propTypes = {
+  actionButtons: PropTypes.func,
   description: PropTypes.string,
   isCondensed: PropTypes.bool,
   handleClose: PropTypes.func,
-  handlePositiveClick: PropTypes.func,
-  negativeButtonProps: PropTypes.shape(Button.propTypes),
-  NotificationIcon: PropTypes.elementType,
-  NotificationIconClassName: PropTypes.string,
-  positiveButtonProps: PropTypes.shape(Button.propTypes),
-  show: PropTypes.bool,
+  headerIcon: PropTypes.node,
   title: PropTypes.string
 };
 
 Notifications.defaultProps = {
-  description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.',
+  actionButtons: null,
+  description:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.',
   isCondensed: false,
   handleClose: () => {},
-  handlePositiveClick: () => {},
-  negativeButtonProps: {},
-  NotificationIcon: CheckCircleIcon,
-  NotificationIconClassName: '',
-  positiveButtonProps: {},
-  show: false,
+  headerIcon: null,
   title: 'Discussion moved'
 };
 
--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,8 +1,30 @@
+const tailwindForms = require('@tailwindcss/forms');
+
 module.exports = {
   content: ['../../packages/bifrost/modules/**/*.{js,jsx}'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        enter: 'enter 200ms ease-out',
+        'slide-in': 'slide-in 1s cubic-bezier(.41,.73,.51,1.02)',
+        leave: 'leave 150ms ease-in forwards'
+      },
+      keyframes: {
+        enter: {
+          '0%': { transform: 'scale(0.9)', opacity: 0 },
+          '100%': { transform: 'scale(1)', opacity: 1 }
+        },
+        leave: {
+          '0%': { transform: 'scale(1)', opacity: 1 },
+          '100%': { transform: 'scale(0.9)', opacity: 0 }
+        },
+        'slide-in': {
+          '0%': { transform: 'translateY(-100%)' },
+          '100%': { transform: 'translateY(0)' }
+        }
+      }
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
@@ -18,7 +40,7 @@ module.exports = {
         600: 'var(--colors-base-600)',
         700: 'var(--colors-base-700)',
         800: 'var(--colors-base-800)',
-        900: 'var(--colors-base-900)',
+        900: 'var(--colors-base-900)'
       },
       brand: {
         50: 'var(--colors-brand-50)',
@@ -30,7 +52,7 @@ module.exports = {
         600: 'var(--colors-brand-600)',
         700: 'var(--colors-brand-700)',
         800: 'var(--colors-brand-800)',
-        900: 'var(--colors-brand-900)',
+        900: 'var(--colors-brand-900)'
       },
       danger: {
         50: 'var(--colors-danger-50)',
@@ -42,7 +64,7 @@ module.exports = {
         600: 'var(--colors-danger-600)',
         700: 'var(--colors-danger-700)',
         800: 'var(--colors-danger-800)',
-        900: 'var(--colors-danger-900)',
+        900: 'var(--colors-danger-900)'
       },
       success: {
         50: 'var(--colors-success-50)',
@@ -54,7 +76,7 @@ module.exports = {
         600: 'var(--colors-success-600)',
         700: 'var(--colors-success-700)',
         800: 'var(--colors-success-800)',
-        900: 'var(--colors-success-900)',
+        900: 'var(--colors-success-900)'
       },
       attention: {
         50: 'var(--colors-attention-50)',
@@ -66,9 +88,9 @@ module.exports = {
         600: 'var(--colors-attention-600)',
         700: 'var(--colors-attention-700)',
         800: 'var(--colors-attention-800)',
-        900: 'var(--colors-attention-900)',
+        900: 'var(--colors-attention-900)'
       },
-      info: {
+      information: {
         50: 'var(--colors-info-50)',
         100: 'var(--colors-info-100)',
         200: 'var(--colors-info-200)',
@@ -78,13 +100,31 @@ module.exports = {
         600: 'var(--colors-info-600)',
         700: 'var(--colors-info-700)',
         800: 'var(--colors-info-800)',
-        900: 'var(--colors-info-900)',
-      },
+        900: 'var(--colors-info-900)'
+      }
     },
+    fontFamily: {
+      sans: [
+        'Inter var',
+        'ui-sans-serif',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ]
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  // eslint-disable-next-line global-require
-  plugins: [require('@tailwindcss/forms')],
+  plugins: [tailwindForms]
 };
