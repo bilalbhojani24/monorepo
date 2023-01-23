ae3860deee106c36bb6ff3ff95129eced1178a1e
samarthBrowserstack <samarth.s@browserstack.com>
2023-01-23 17:34:29 +0530

    Steps revamp

--- packages/bifrost/modules/Steps/Steps.stories.jsx
+++ packages/bifrost/modules/Steps/Steps.stories.jsx
@@ -1,19 +1,41 @@
 import React from 'react';
-import Steps from './index';
+
 import DocPageTemplate from '../../.storybook/DocPageTemplate';
-import { STEPS_STATUS, STEPS_FORMAT } from './const/stepsConstants';
+
+import { STEPS_FORMAT, STEPS_STATUS } from './const/stepsConstants';
+import Steps from './index';
 
 const steps = [
-  { id: '1', name: 'Create account', description: 'Vitae sed mi luctus laoreet.', status: STEPS_STATUS[0] },
+  {
+    id: '1',
+    name: 'Create account',
+    description: 'Vitae sed mi luctus laoreet.',
+    status: STEPS_STATUS[0],
+  },
   {
     id: '2',
     name: 'Profile information',
     description: 'Cursus semper viverra facilisis et et some more.',
     status: STEPS_STATUS[1],
   },
-  { id: '3', name: 'Business information', description: 'Penatibus eu quis ante.', status: STEPS_STATUS[2] },
-  { id: '4', name: 'Theme', description: 'Faucibus nec enim leo et.', status: STEPS_STATUS[2] },
-  { id: '5', name: 'Preview', description: 'Iusto et officia maiores porro ad non quas.', status: STEPS_STATUS[2] },
+  {
+    id: '3',
+    name: 'Business information',
+    description: 'Penatibus eu quis ante.',
+    status: STEPS_STATUS[2],
+  },
+  {
+    id: '4',
+    name: 'Theme',
+    description: 'Faucibus nec enim leo et.',
+    status: STEPS_STATUS[2],
+  },
+  {
+    id: '5',
+    name: 'Preview',
+    description: 'Iusto et officia maiores porro ad non quas.',
+    status: STEPS_STATUS[2],
+  },
 ];
 
 const defaultConfig = {
@@ -21,9 +43,11 @@ const defaultConfig = {
   component: Steps,
   parameters: {
     docs: {
-      page: () => {
-        return <DocPageTemplate importStatement={"import Steps from 'bifrost/Steps'"} />;
-      },
+      page: () => (
+        <DocPageTemplate
+          importStatement={"import Steps from 'bifrost/Steps'"}
+        />
+      ),
     },
   },
   argTypes: {
@@ -51,14 +75,61 @@ const defaultConfig = {
       type: { summary: 'STRING', required: false },
       defaultValue: STEPS_FORMAT[0],
     },
+    wrapperClassName: {
+      type: { summary: 'STRING', required: false },
+      description: 'Provide class to overall Step component',
+      control: { type: 'text' },
+      defaultValue: '',
+    },
   },
   controls: {},
 };
-const Template = (args) => <Steps {...args} />;
-const Primary = Template.bind({});
-Primary.parameters = {
+
+const Bullet = (args) => <Steps {...args} />;
+const BulletList = Bullet.bind({});
+Bullet.parameters = {
+  controls: {},
+};
+
+BulletList.args = {
+  format: STEPS_FORMAT[1],
+};
+
+const Panels = (args) => <Steps {...args} />;
+const PanelsList = Panels.bind({});
+Panels.parameters = {
+  controls: {},
+};
+
+PanelsList.args = {
+  format: STEPS_FORMAT[2],
+};
+
+const Circle = (args) => <Steps {...args} />;
+const CircleList = Circle.bind({});
+Circle.parameters = {
+  controls: {},
+};
+
+CircleList.args = {
+  format: STEPS_FORMAT[3],
+};
+
+const BulletsAndText = (args) => <Steps {...args} />;
+const BulletsAndTextList = BulletsAndText.bind({});
+BulletsAndText.parameters = {
+  controls: {},
+};
+
+BulletsAndTextList.args = {
+  format: STEPS_FORMAT[4],
+};
+
+const Simple = (args) => <Steps {...args} />;
+const SimpleList = Simple.bind({});
+Simple.parameters = {
   controls: {},
 };
 
 export default defaultConfig;
-export { Primary };
+export { BulletList, BulletsAndTextList, CircleList, PanelsList, SimpleList };
