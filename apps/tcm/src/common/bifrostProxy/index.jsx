6f1869f85c6b6325ddf4850da35c2e85bcbf6e97
ribinBS <ribin@browserstack.com>
2023-02-01 19:39:11 +0530

    pagination proxied

--- apps/tcm/src/common/bifrostProxy/index.jsx
+++ apps/tcm/src/common/bifrostProxy/index.jsx
@@ -21,6 +21,7 @@ import {
   ModalHeader,
   Notifications,
   PageHeadings,
+  Pagination,
   RadioGroup,
   SectionHeadings,
   SelectMenu,
@@ -51,6 +52,7 @@ export const TMPageHeadings = (props) => (
 );
 export const TMTabs = (props) => <Tabs {...props} />;
 export const TMListTree = (props) => <ListTree {...props} />;
+export const TMPagination = (props) => <Pagination {...props} />;
 export const TMListTreeNode = (props) => <ListTreeNode {...props} />;
 export const TMListTreeNodeContents = (props) => (
   <ListTreeNodeContents {...props} />
