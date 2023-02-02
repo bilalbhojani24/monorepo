97c5c394ac7a202a2d46b09b13b9165c54d3a3e0
kshitij awadhiya <kawadhiya21@gmail.com>
2023-02-02 15:53:16 +0530

    one line change

--- apps/tcm/src/features/Projects/components/AllProjects.jsx
+++ apps/tcm/src/features/Projects/components/AllProjects.jsx
@@ -55,7 +55,7 @@ const AllProjects = () => {
           role="button"
           className="hover:text-brand-600 cursor-pointer"
           tabIndex={0}
-          onClick={handleClickDynamicLink(AppRoute.TEST_CASES, rowData.id)}
+          onClick={rowData.test_cases_count > 0 ? handleClickDynamicLink(AppRoute.DASHBOARD, rowData.id) : handleClickDynamicLink(AppRoute.TEST_CASES, rowData.id)}
           onKeyDown={handleClickDynamicLink(AppRoute.TEST_CASES, rowData.id)}
         >
           <div className="text-base-900 hover:text-brand-600 font-medium ">
