13d63f00bfec3ca24ab3fd4bfccab9b96b3c86b7
ribinBS <ribin@browserstack.com>
2023-02-02 14:28:50 +0530

    empty state layout fixed

--- apps/tcm/src/features/Repository/components/TestCases.jsx
+++ apps/tcm/src/features/Repository/components/TestCases.jsx
@@ -33,71 +33,75 @@ export default function TestCases() {
 
   return (
     <div className="flex w-full flex-col items-start">
-      <div className="border-base-300 flex w-full items-start border-b py-3 pr-3">
-        <div className="w-full">
-          <TMInputField
-            placeholder="Search by Test Case name, ID"
-            leadingIcon={<SearchIcon className="text-base-400" />}
-          />
-        </div>
-        <TMButton
-          buttonType="half-rounded-button"
-          wrapperClassName="ml-3"
-          size="default"
-          variant="primary"
-          colors="white"
-        >
-          Filter
-        </TMButton>
-      </div>
-      <div className="border-base-300 flex w-full flex-1 flex-col items-stretch border-l">
-        {selectedFolder && (
-          <div className="border-base-200 w-full border-b p-4">
-            <div className="text-base-800 w-full font-medium">
-              {selectedFolder?.name}
-              <TMTooltip
-                size="xs"
-                placementSide="bottom"
-                theme="dark"
-                content={
-                  <>
-                    <TMTooltipHeader>{selectedFolder?.name}</TMTooltipHeader>
-                    <TMTooltipBody>
-                      <p className="text-sm">
-                        URL: {selectedFolder?.links?.self || ''}
-                      </p>
-                    </TMTooltipBody>
-                  </>
-                }
-              >
-                <InfoOutlinedIcon className="ml-1 !h-3.5 !w-3.5" />
-              </TMTooltip>
-            </div>
-            {selectedFolder?.notes && (
-              <div className="text-base-500 mt-1 text-xs">
-                {selectedFolder?.notes}
-              </div>
-            )}
-          </div>
-        )}
-
-        {allTestCases.length ? (
-          <>
-            <div className="max-h-[calc(100vh-13.5rem)] flex-1 flex-col items-stretch  overflow-y-auto">
-              <TestCasesTable
-                isCondensed
-                containerWrapperClass="md:rounded-none"
-                rows={allTestCases}
+      {allTestCases.length ? (
+        <>
+          <div className="border-base-300 flex w-full items-start border-b py-3 pr-3">
+            <div className="w-full">
+              <TMInputField
+                placeholder="Search by Test Case name, ID"
+                leadingIcon={<SearchIcon className="text-base-400" />}
               />
             </div>
-            <InlineAddTestCase />
-          </>
-        ) : (
-          <div className="flex w-full flex-1 items-center justify-center">
-            <BlankPage />
+            <TMButton
+              buttonType="half-rounded-button"
+              wrapperClassName="ml-3"
+              size="default"
+              variant="primary"
+              colors="white"
+            >
+              Filter
+            </TMButton>
           </div>
-        )}
-      </div>
+          <div className="border-base-300 flex w-full flex-1 flex-col items-stretch border-l">
+            {selectedFolder && (
+              <div className="border-base-200 w-full border-b p-4">
+                <div className="text-base-800 w-full font-medium">
+                  {selectedFolder?.name}
+                  <TMTooltip
+                    size="xs"
+                    placementSide="bottom"
+                    theme="dark"
+                    content={
+                      <>
+                        <TMTooltipHeader>
+                          {selectedFolder?.name}
+                        </TMTooltipHeader>
+                        <TMTooltipBody>
+                          <p className="text-sm">
+                            URL: {selectedFolder?.links?.self || ''}
+                          </p>
+                        </TMTooltipBody>
+                      </>
+                    }
+                  >
+                    <InfoOutlinedIcon className="ml-1 !h-3.5 !w-3.5" />
+                  </TMTooltip>
+                </div>
+                {selectedFolder?.notes && (
+                  <div className="text-base-500 mt-1 text-xs">
+                    {selectedFolder?.notes}
+                  </div>
+                )}
+              </div>
+            )}
+            <>
+              <div className="max-h-[calc(100vh-13.5rem)] flex-1 flex-col items-stretch  overflow-y-auto">
+                <TestCasesTable
+                  isCondensed
+                  containerWrapperClass="md:rounded-none"
+                  rows={allTestCases}
+                />
+              </div>
+              <InlineAddTestCase />
+            </>
+          </div>
+        </>
+      ) : (
+        <div className="border-base-300 flex w-full flex-1 items-center justify-center border-l">
+          <BlankPage />
+        </div>
+      )}
+
       <DeleteTestCase show={showDeleteModal} />
     </div>
   );
