1975f32cca6f5ba8462b0512249ab6550a5c1ef9
ribinBS <ribin@browserstack.com>
2023-02-07 11:28:17 +0530

    local duplicate tag issue fixed

--- apps/tcm/src/common/AddTagModal/components/useAddTagModal.js
+++ apps/tcm/src/common/AddTagModal/components/useAddTagModal.js
@@ -5,28 +5,34 @@ const useAddTagModal = ({
   isVisible,
   onClose,
   verifierFunction,
-  existingTags,
+  existingTags
 }) => {
   const [allTags, setAllTags] = useState(existingTags);
   const [newTags, setNewTags] = useState([]);
   const [enteredTag, setTagEntered] = useState('');
   const [errorText, setErrorText] = useState(null);
 
+  const isLocalDuplicateExists = (tagsSplitted) =>
+    tagsSplitted.find((item) => allTags.includes(item));
+
   const addTagHandler = () => {
     setErrorText(null);
     const tagsSplitted = splitStringToArray(enteredTag, ',');
-    verifierFunction(tagsSplitted).then((data) => {
-      if (data) {
-        if (data?.tags) {
-          setNewTags([...newTags, ...data?.tags]);
-          setAllTags([...new Set([...allTags, ...data?.tags])]);
-        }
-        setTagEntered(data?.error_tags?.join(', ') || '');
-      }
 
-      if (data?.error_tags?.length)
-        setErrorText('A tag with the same name already exists');
-    });
+    if (!isLocalDuplicateExists(tagsSplitted)) {
+      verifierFunction(tagsSplitted).then((data) => {
+        if (data) {
+          if (data?.tags) {
+            setNewTags([...newTags, ...data?.tags]);
+            setAllTags([...new Set([...allTags, ...data?.tags])]);
+          }
+          setTagEntered(data?.error_tags?.join(', ') || '');
+        }
+
+        if (data?.error_tags?.length)
+          setErrorText('A tag with the same name already exists');
+      });
+    } else setErrorText('A tag with the same name already exists');
   };
 
   const onTagRemoveClick = (tag) => {
@@ -66,6 +72,7 @@ const useAddTagModal = ({
     addTagHandler,
     onTagRemoveClick,
     onCloseHandler,
+    setErrorText
   };
 };
 export default useAddTagModal;
