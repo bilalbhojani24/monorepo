b79ac957dc710942eac92f61385b3f02ba476b9b
ribinBS <ribin@browserstack.com>
2023-02-08 20:19:46 +0530

    RTE uploader WIP

--- apps/tcm/src/api/attachments.api.js
+++ apps/tcm/src/api/attachments.api.js
@@ -2,3 +2,14 @@ import { fetchPost } from './_utils/fetch';
 
 export const uploadFilesAPI = async ({ projectId, payload }) =>
   fetchPost(`/api/v1/projects/${projectId}/generic/attachments`, payload);
+
+export const imageUploadRTEHandlerAPI = async ({ projectId, blobInfo }) => {
+  const formData = new FormData();
+  const files = blobInfo.blob();
+  formData.append('attachments[]', files);
+
+  return fetchPost(
+    `/api/v1/projects/${projectId}/generic/rte_upload`,
+    formData
+  );
+};
