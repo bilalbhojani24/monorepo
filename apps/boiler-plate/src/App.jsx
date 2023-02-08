9c7c105a38e3580ea3ef3b69d270cb63fdd76f03
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-02-08 13:25:55 +0530

    added mock variant

--- apps/boiler-plate/src/App.jsx
+++ apps/boiler-plate/src/App.jsx
@@ -11,9 +11,15 @@ const initAPI = async () => {
     }, 2000);
   });
 
+  // returns status code - 200 (uncomment and test)
   return axios.get(
-    'https://run.mocky.io/v3/a1656866-98fe-49cd-9b97-1163c2866b48'
+    'https://run.mocky.io/v3/ae5ce0d2-cecc-4580-8bdb-a91cd9d8db94'
   );
+
+  // returns status code - 401 (uncomment and test)
+  // return axios.get(
+  //   'https://run.mocky.io/v3/a1656866-98fe-49cd-9b97-1163c2866b48'
+  // );
 };
 
 const App = () => {
