6d57d54e0f540450c9cc61c7737ae99691b80c84
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-02-09 16:33:29 +0530

    stats rearch, added onClick to stats

--- packages/bifrost/modules/Stats/const/statsConstants.js
+++ packages/bifrost/modules/Stats/const/statsConstants.js
@@ -1,5 +1,3 @@
-export const STATS_SPACING = ['spaced', 'shared-borders'];
-
 export const STATS_VARIANTS = {
   WITH_ICON: 'withIcon',
   WITHOUT_ICON: 'withoutIcon',
--- packages/bifrost/modules/TableRow/index.jsx
+++ packages/bifrost/modules/TableRow/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
 import { twClassNames } from '@browserstack/utils';
+import PropTypes from 'prop-types';
 
 import './styles.scss';
 
