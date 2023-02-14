6e35a7d78e1645be99492dce2732f292654e5b1b
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-02-14 15:24:23 +0530

    SelectMenu label

--- packages/bifrost/modules/ComboboxOptions/index.jsx
+++ packages/bifrost/modules/ComboboxOptions/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { forwardRef, useContext } from 'react';
 import { twClassNames } from '@browserstack/utils';
 import { Combobox } from '@headlessui/react';
 import { oneOf } from 'prop-types';
@@ -15,82 +15,85 @@ import { CheckIcon } from '../Icon';
 
 import './styles.scss';
 
-const ComboboxOptions = ({ checkPosition, option, wrapperClassName }) => {
-  const { isMulti } = useContext(ComboboxContextData);
+const ComboboxOptions = forwardRef(
+  ({ option, checkPosition, wrapperClassName }, ref) => {
+    const { isMulti } = useContext(ComboboxContextData);
 
-  return (
-    <Combobox.Option
-      key={option.value}
-      value={option}
-      className={({ active }) =>
-        twClassNames(
-          'relative cursor-pointer select-none py-2 pl-3 pr-9',
-          active && !isMulti ? 'bg-brand-600 text-white' : 'text-base-900',
-          {
-            'py-2 pl-3 pr-9': checkPosition === CHECK_POSITION[1] && !isMulti,
-            'py-2 pl-8 pr-4': checkPosition === CHECK_POSITION[0] && !isMulti,
-            'hover:bg-base-50 py-2 pl-2 cursor-pointer': isMulti
-          },
-          wrapperClassName
-        )
-      }
-    >
-      {({ active, selected }) => (
-        <>
-          {!isMulti ? (
-            <>
-              <div className="flex items-center">
-                {option?.image && (
-                  <img
-                    src={option.image}
-                    alt=""
-                    className="mr-3 h-6 w-6 shrink-0 rounded-full"
-                  />
+    return (
+      <Combobox.Option
+        ref={ref}
+        key={option.value}
+        value={option}
+        className={({ active }) =>
+          twClassNames(
+            'relative cursor-pointer select-none py-2 pl-3 pr-9',
+            active && !isMulti ? 'bg-brand-600 text-white' : 'text-base-900',
+            {
+              'py-2 pl-3 pr-9': checkPosition === CHECK_POSITION[1] && !isMulti,
+              'py-2 pl-8 pr-4': checkPosition === CHECK_POSITION[0] && !isMulti,
+              'hover:bg-base-50 py-2 pl-2 cursor-pointer': isMulti
+            },
+            wrapperClassName
+          )
+        }
+      >
+        {({ active, selected }) => (
+          <>
+            {!isMulti ? (
+              <>
+                <div className="flex items-center">
+                  {option?.image && (
+                    <img
+                      src={option.image}
+                      alt=""
+                      className="mr-3 h-6 w-6 shrink-0 rounded-full"
+                    />
+                  )}
+                  <span
+                    className={twClassNames(
+                      'block truncate',
+                      selected && 'font-semibold'
+                    )}
+                  >
+                    {option.label}
+                  </span>
+                </div>
+                {selected && (
+                  <span
+                    className={twClassNames(
+                      'absolute inset-y-0 right-0 flex items-center pr-4',
+                      active ? 'text-white' : 'text-brand-600',
+                      {
+                        'right-0 pr-4':
+                          checkPosition === CHECK_POSITION[1] || option?.image,
+                        'left-0 pl-1.5': checkPosition === CHECK_POSITION[0]
+                      }
+                    )}
+                  >
+                    <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                  </span>
                 )}
-                <span
-                  className={twClassNames(
-                    'block truncate',
-                    selected && 'font-semibold'
-                  )}
-                >
+              </>
+            ) : (
+              <div className="flex items-center space-x-2">
+                <input
+                  type="checkbox"
+                  checked={selected}
+                  id={option.name}
+                  className="border-base-300 text-brand-600 focus:ring-brand-500 h-4 w-4 cursor-pointer rounded"
+                  readOnly
+                />
+                <label htmlFor={option.name} className="cursor-pointer">
                   {option.label}
-                </span>
+                </label>
               </div>
-              {selected && (
-                <span
-                  className={twClassNames(
-                    'absolute inset-y-0 right-0 flex items-center pr-4',
-                    active ? 'text-white' : 'text-brand-600',
-                    {
-                      'right-0 pr-4':
-                        checkPosition === CHECK_POSITION[1] || option?.image,
-                      'left-0 pl-1.5': checkPosition === CHECK_POSITION[0]
-                    }
-                  )}
-                >
-                  <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                </span>
-              )}
-            </>
-          ) : (
-            <div className="flex items-center space-x-2">
-              <input
-                type="checkbox"
-                checked={selected}
-                id={option.name}
-                className="border-base-300 text-brand-600 focus:ring-brand-500 h-4 w-4 cursor-pointer rounded"
-                readOnly
-              />
-              <label htmlFor={option.name} className="cursor-pointer">
-                {option.label}
-              </label>
-            </div>
-          )}
-        </>
-      )}
-    </Combobox.Option>
-  );
-};
+            )}
+          </>
+        )}
+      </Combobox.Option>
+    );
+  }
+);
 
 ComboboxOptions.propTypes = {
   checkPosition: oneOf(CHECK_POSITION),
