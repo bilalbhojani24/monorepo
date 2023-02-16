971a050cfb5cca896d76a2e965af05a5f3864c9b
Vishal Sanserwal <vishal.san@browserstack.com>
2023-02-16 09:08:10 +0530

    loader added

---
+++ apps/o11y/src/common/Loader/components/Loader.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import className from 'classnames';
+import PropTypes from 'prop-types';
+
+import LoaderIcon from '../../../assets/icons/loader.svg';
+
+const Loader = ({ isLoading, isOverlayed, wrapperClassName }) => {
+  if (!isLoading) return '';
+
+  return (
+    <div
+      className={className(
+        'flex justify-center items-center  w-full min-h-min',
+        wrapperClassName,
+        {
+          'absolute left-0 top-0': isOverlayed
+        }
+      )}
+    >
+      <img src={LoaderIcon} alt="" className="text-base-400 animate-spin" />
+    </div>
+  );
+};
+
+Loader.propTypes = {
+  wrapperClassName: PropTypes.string,
+  isLoading: PropTypes.bool,
+  isOverlayed: PropTypes.bool
+};
+
+Loader.defaultProps = {
+  wrapperClassName: '',
+  isLoading: true,
+  isOverlayed: false
+};
+
+export default Loader;
