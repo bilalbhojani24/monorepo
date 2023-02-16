d7940cd7b6e311ccc6406a6ae429d17e4759f59f
Vishal Sanserwal <vishal.san@browserstack.com>
2023-02-16 08:54:24 +0530

    sanitized app .jsx

--- apps/o11y/src/App.jsx
+++ apps/o11y/src/App.jsx
@@ -1,30 +1,16 @@
-import React from 'react';
 import { useAuthRoutes } from '@browserstack/hooks';
 import axios from 'axios';
 
 import { versionedBaseRoute } from './constants/common';
 import { APP_ROUTES } from './constants/routesConstants';
 
-const initAPI = () =>
-  // await new Promise((resolve) => {
-  //   setTimeout(() => {
-  //     resolve(false);
-  //   }, 2000);
-  // });
+const initAPI = () => axios.get(`${versionedBaseRoute()}/projects/lite`);
 
-  // returns status code - 200 (uncomment and test)
-  axios.get(`${versionedBaseRoute()}/projects/lite`);
-
-// returns status code - 401 (uncomment and test)
-// axios.get('https://run.mocky.io/v3/a1656866-98fe-49cd-9b97-1163c2866b48');
-const App = () => {
-  const Routes = useAuthRoutes(
+const App = () =>
+  useAuthRoutes(
     APP_ROUTES,
     initAPI,
     'https://www.browserstack.com/users/sign_in'
   );
-
-  return <>{Routes}</>;
-};
 
 export default App;
