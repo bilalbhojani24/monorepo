f0d58186f56ba0b49479dd3c313135721424192a
Bipul Jain <bipul@browserstack.com>
2023-02-16 13:39:27 +0530

    Adding Blank Pages and Tags formatting

--- apps/tcm/src/features/Repository/components/BlankPage.jsx
+++ apps/tcm/src/features/Repository/components/BlankPage.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
-import { SourceOutlinedIcon } from 'assets/icons';
-import { TMEmptyState } from 'common/bifrostProxy';
+import { SourceOutlinedIcon,
+    // FileDownloadIcon
+} from 'assets/icons';
+import { TMEmptyState, TMButton } from 'common/bifrostProxy';
+import {MdSaveAlt, MdOutlineDescription} from '@browserstack/bifrost'
+import AppRoute from 'const/routes';
+import { Link } from 'react-router-dom';
 
 import InlineAddTestCase from './InlineAddTestCase';
 
@@ -16,6 +21,40 @@ export default function BlankPage() {
       <div className="w-9/12 p-5">
         <InlineAddTestCase noBorder />
       </div>
+      <div className="w-9/12 p-5 text-base-500 text-sm text-center">
+          <p>Alternatively, you can import your test case data using below options</p>
+      </div>
+        {/*<p>Alternatively, you can import your test case data using below options</p>*/}
+
+        <div className={"flex justify-center"}>
+            <div className="flex-1">
+                <Link to={AppRoute.IMPORT}>
+                    <TMButton
+                        wrapperClassName="ml-4 whitespace-nowrap w-64 py-3"
+                        variant="rounded"
+                        colors="white"
+                        size="large"
+                        icon={<MdSaveAlt/>}
+                    >
+                        &nbsp;Quick Import
+                    </TMButton>
+                </Link>
+
+            </div>
+            <div className="flex-1">
+                <Link to={AppRoute.IMPORT_CSV}>
+                    <TMButton
+                        wrapperClassName="ml-4 whitespace-nowrap w-64 py-3"
+                        variant="rounded"
+                        colors={"white"}
+                        size={"large"}
+                        icon={<MdOutlineDescription/>}
+                    >
+                        &nbsp;Import from CSV / XLS
+                    </TMButton>
+                </Link>
+            </div>
+        </div>
     </div>
   );
 }
