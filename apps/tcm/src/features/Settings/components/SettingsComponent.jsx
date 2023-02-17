dcaf7dd814058705b67d12537a51e7db6fcb8062
Bipul Jain <bipul@browserstack.com>
2023-02-17 12:49:57 +0530

    Adding changes

--- apps/tcm/src/features/Settings/components/SettingsComponent.jsx
+++ apps/tcm/src/features/Settings/components/SettingsComponent.jsx
@@ -34,6 +34,11 @@ const SettingsComponent = () => {
                         onTabChange={handleTabChange}
                     />
                 </div>
+                <div className="flex flex-1 shrink-0  grow flex-col overflow-y-auto p-4">
+                    <div className="border-base-200 flex grow flex-col justify-start  rounded-md border bg-white">
+                        Hello
+                    </div>
+                </div>
             </div>
         </div>
     );
