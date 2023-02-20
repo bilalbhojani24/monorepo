7c2e8673301f35da498828efe6a5e0e165c904da
ribinBS <ribin@browserstack.com>
2023-02-20 12:29:38 +0530

    import CSV buttons

--- apps/tcm/src/features/Repository/components/TopSection.jsx
+++ apps/tcm/src/features/Repository/components/TopSection.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { TMButton, TMPageHeadings } from 'common/bifrostProxy';
+import { Link, useNavigate } from 'react-router-dom';
+import { MdOutlineExpandMore } from '@browserstack/bifrost';
+import { TMButton, TMDropdown, TMPageHeadings } from 'common/bifrostProxy';
+import AppRoute from 'const/routes';
+
+import { IMPORT_OPTIONS } from '../const/topSectionConst';
 
 import useAddEditTestCase from './useAddEditTestCase';
 
 const TopSection = () => {
+  const navigate = useNavigate();
   const { showTestCaseAdditionPage, isAddTestCasePageVisible } =
     useAddEditTestCase();
 
@@ -22,6 +28,30 @@ const TopSection = () => {
                 Create Test Case
               </TMButton>
             )}
+            <div className="flex">
+              <Link to={AppRoute.IMPORT_CSV} className="flex flex-1">
+                <TMButton
+                  variant="primary"
+                  size="default"
+                  colors="white"
+                  // onClick={showAddProjectModal}
+                  wrapperClassName="ml-3 whitespace-nowrap w-full rounded-tr-none rounded-br-none focus:ring-offset-0 focus:z-10"
+                >
+                  Import via CSV
+                </TMButton>
+              </Link>
+              <TMDropdown
+                triggerIcon={
+                  <MdOutlineExpandMore className="text-2xl text-black" />
+                }
+                triggerClassName="rounded-tl-none rounded-bl-none focus:ring-offset-0 focus:z-10 border-l-0"
+                triggerVariant="menu-button"
+                options={IMPORT_OPTIONS}
+                onClick={(e, selectedOption) =>
+                  selectedOption?.route ? navigate(selectedOption?.route) : null
+                }
+              />
+            </div>
           </>
         }
       />
