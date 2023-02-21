53eaca11ed0e7147d435cc827e7b589bae25291d
ribinBS <ribin@browserstack.com>
2023-02-21 18:41:06 +0530

    tooltip removed for stacked chart

--- apps/tcm/src/features/Dashboard/components/chartHelpers.js
+++ apps/tcm/src/features/Dashboard/components/chartHelpers.js
@@ -156,21 +156,21 @@ export const barOptionsCreator = ({
       text: null
     }
   },
-  tooltip: {
-    headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-    pointFormat:
-      '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-      '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-    footerFormat: '</table>',
-    shared: true,
-    useHTML: true
-  },
+  // tooltip: {
+  //   headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+  //   pointFormat:
+  //     '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+  //     '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+  //   footerFormat: '</table>',
+  //   shared: true,
+  //   useHTML: true
+  // },
   plotOptions: {
     series: {
       color: '#465FA3'
     },
     column: {
-      pointPadding: 0.2,
+      // pointPadding: 0.2,
       borderWidth: 0
     }
   },
