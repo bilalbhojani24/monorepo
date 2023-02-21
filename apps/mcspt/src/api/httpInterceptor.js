9a3c93bc1cd19a249c1ca29cc9247780d67523d5
Ashu <ashutosh.d@browserstack.com>
2023-02-21 13:52:33 +0530

    correcting interceptor

--- apps/mcspt/src/api/httpInterceptor.js
+++ apps/mcspt/src/api/httpInterceptor.js
@@ -2,9 +2,11 @@ import axios from 'axios';
 
 // Interceptors for api calls. This was not added in the main axios interceptors; didn't want to break things there!
 axios.interceptors.request.use((config) => {
-  const interceptedConfig = { config };
+  const interceptedConfig = { ...config };
 
-  interceptedConfig.headers.Accept = 'application/json';
+  if (interceptedConfig?.headers?.['Content-Type']) {
+    interceptedConfig.headers['Content-Type'] = 'application/json';
+  }
 
   return interceptedConfig;
 });
