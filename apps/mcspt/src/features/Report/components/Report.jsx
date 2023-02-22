bb90d10ccaad2a1e1f00aa5959ef650e55eb57a9
Ashu <ashutosh.d@browserstack.com>
2023-02-22 19:56:05 +0530

    adding spacing adjustments

--- apps/mcspt/src/features/Report/components/Report.jsx
+++ apps/mcspt/src/features/Report/components/Report.jsx
@@ -10,20 +10,22 @@ const Report = () => {
   const { sessionData } = useReport();
 
   return (
-    <div id="reportContentContainer" className="flex min-h-screen flex-col">
+    <div id="reportContainer" className="flex min-h-screen flex-col">
       <ReportHeader />
 
       <div className="flex max-w-full flex-1">
         <div className="flex w-64 shrink-0 grow-0 flex-col items-center lg:w-64 xl:w-[360px]">
-          <div className="bg-base-100 mx-3 h-full w-full">
+          <div className="bg-base-100 h-full w-full p-2">
             <MediaPlayer
+              wrapperClassName="rounded-lg overflow-hidden"
               url={`securefileprotocol://${sessionData?.metadata?.video}`}
               controlPanelStickToBottom
               controlPanelClassName="fixed z-20"
             />
           </div>
         </div>
-        <div id="reportContentContainer" className="flex-1">
+
+        <div id="reportContentContainer" className="mb-4 flex-1">
           <ReportContent />
         </div>
       </div>
