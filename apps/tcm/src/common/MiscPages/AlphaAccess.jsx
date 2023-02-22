0ef36d4c503f4600520628c863dd9a19b1ce20df
ribinBS <ribin@browserstack.com>
2023-02-22 19:31:26 +0530

    request access WIP

--- apps/tcm/src/common/MiscPages/AlphaAccess.jsx
+++ apps/tcm/src/common/MiscPages/AlphaAccess.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 import { MdLock, MdMail, MdOutlineTextSnippet } from '@browserstack/bifrost';
+import { requestAccessAPI } from 'api/common.api';
 import { TMButton, TMEmptyState } from 'common/bifrostProxy';
-import AppRoute from 'const/routes';
 
 const AlphaAccess = () => {
-  const navigate = useNavigate();
-
-  const goToProject = () => {
-    navigate(AppRoute.ROOT);
+  const requestAccess = () => {
+    requestAccessAPI().then((data) => {
+      debugger;
+    });
   };
 
   return (
@@ -23,14 +22,20 @@ const AlphaAccess = () => {
           buttonProps={null}
         />
         <div className="mt-6 flex gap-4">
+          <a href="https://www.browserstack.com/docs/test-management">
+            <TMButton
+              size="default"
+              colors="white"
+              icon={<MdOutlineTextSnippet className="h-5 w-5" />}
+            >
+              View Documentation
+            </TMButton>
+          </a>
           <TMButton
             size="default"
-            colors="white"
-            icon={<MdOutlineTextSnippet className="h-5 w-5" />}
+            icon={<MdMail className="h-5 w-5" />}
+            onClick={requestAccess}
           >
-            View Documentation
-          </TMButton>
-          <TMButton size="default" icon={<MdMail className="h-5 w-5" />}>
             Request Access
           </TMButton>
         </div>
