ce72401d9c60fac7b5d3f2f5d3adb80d84aab586
ribinBS <ribin@browserstack.com>
2023-02-24 14:48:45 +0530

    route updates

--- apps/tcm/src/const/routes.js
+++ apps/tcm/src/const/routes.js
@@ -7,9 +7,8 @@ const AppRoute = {
   TEST_CASES_SEARCH: '/projects/:projectId/folder/search',
   DASHBOARD: '/projects/:projectId',
   TEST_RUNS: '/projects/:projectId/test-runs',
-  TEST_RUN_DETAILS: '/projects/:projectId/test-runs/:testRunId/test-cases',
-  TEST_RUN_ISSUES:
-    '/projects/:projectId/test-runs/:testRunId/test-cases/issues',
+  TEST_RUN_DETAILS: '/projects/:projectId/test-runs/:testRunId',
+  TEST_RUN_ISSUES: '/projects/:projectId/test-runs/:testRunId/issues',
   TEST_RUNS_EDIT: '/projects/:projectId/test-runs/:testRunId/edit',
   ONBOARDING: '/onboarding',
   RESOURCES: '/resources',
