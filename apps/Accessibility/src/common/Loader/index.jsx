df295cd978d449b603a4d016c2ab21f8ac2cf7cb
Swaraj Gandhi <swaraj@browserstack.com>
2023-02-24 11:56:00 +0530

    added loader for the screen

--- apps/Accessibility/src/common/Loader/index.jsx
+++ apps/Accessibility/src/common/Loader/index.jsx
@@ -3,7 +3,7 @@ import LoaderImage from 'assets/blue-spinner.svg';
 
 export default function Loader() {
   return (
-    <div className="flex h-full items-center justify-center">
+    <div className="flex h-screen items-center justify-center">
       <img src={LoaderImage} alt="loading" />
     </div>
   );
