c5287025a59979fa8c8d8f5ce5f5dc0ced1e81dc
ribinBS <ribin@browserstack.com>
2023-03-01 17:48:55 +0530

    texteditorSkin

--- apps/tcm/src/common/bifrostProxy/components/TMRichTextEditor.jsx
+++ apps/tcm/src/common/bifrostProxy/components/TMRichTextEditor.jsx
@@ -13,7 +13,11 @@ const TMRichTextEditor = (props) => {
     <RichTextEditor
       {...props}
       onAssetUpload={imageUploadRTEHelper}
-      assetsURL={import.meta.env.DEV ? '/dist/assets/css' : '/assets/css'}
+      assetsURL={
+        import.meta.env.DEV
+          ? '/dist/assets/css/texteditorSkin'
+          : '/assets/css/texteditorSkin'
+      }
     />
   );
 };
