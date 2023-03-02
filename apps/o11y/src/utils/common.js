1721fbd5424f9338a04e82e58c2a67b515131653
Vishal Sanserwal <vishal.san@browserstack.com>
2023-03-02 21:58:08 +0530

    updated to take care od hyphen seperated production products

--- apps/o11y/src/utils/common.js
+++ apps/o11y/src/utils/common.js
@@ -1,10 +1,11 @@
 export const getBaseUrl = () => {
-  const url = new URL(window.location);
-  let env = url.hostname.split('.')[0].split('-').slice(1).join('-');
-  env = env ? `${env}.` : '';
-  const splitHost = url.hostname.split('.');
-  splitHost.shift();
-  return `${url.protocol}//${env}${splitHost.join('.')}`;
+  const { hostname, protocol } = window.location;
+  const hostnameParts = hostname.split('.');
+  let env = hostnameParts[0].split('-').slice(1).join('-');
+  hostnameParts.shift();
+  const domain = hostnameParts.join('.');
+  env = env && domain === 'bsstag.com' ? `${env}.` : '';
+  return `${protocol}//${env}${domain}`;
 };
 export const getDocUrl = (path) =>
   `${getBaseUrl()}/docs/test-observability/${path}`;
