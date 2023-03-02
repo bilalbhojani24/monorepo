3c39d5860b94c37bc161cd2578b03bab135f0080
rajeev-bstack <rajeev@browserstack.com>
2023-03-02 14:29:42 +0530

    INTG - 21 | @rajeev-bstack | Implement Refresh token flow

--- packages/integrations/src/api/interceptor.js
+++ packages/integrations/src/api/interceptor.js
@@ -5,9 +5,9 @@ import { store } from '../features/store';
 
 import { fetchTokenThunk } from './fetchToken';
 // Add a request interceptor
+const cookie = new Cookie();
 export const requestInterceptor = axios.interceptors.request.use(
   (config) => {
-    const cookie = new Cookie();
     const token = cookie.read('UAT');
     if (token) {
       // eslint-disable-next-line no-param-reassign
@@ -29,9 +29,19 @@ export const responseInterceptor = axios.interceptors.response.use(
     // Do something with response error
     const { status, data } = error.response;
     if (status === 401 && data.message.name === 'TokenExpiredError') {
-      // token has expired, refetch it
-      // console.log(dispatch);
-      store.dispatch(fetchTokenThunk());
+      // token has expired
+      cookie.erase('UAT'); // remove cookie
+      return store.dispatch(fetchTokenThunk()).then(() => {
+        // new UAT has been issued and stored in cookie
+        const token = cookie.read('UAT');
+        const configShallowCopy = error.config;
+        if (token) {
+          // update the original request config with new token
+          configShallowCopy.headers.Authorization = `Bearer ${token}`;
+        }
+        // make new request with updated token
+        return axios.request(configShallowCopy);
+      });
     }
     return Promise.reject(error);
   }
