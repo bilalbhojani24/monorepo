23cc487668651f2299bff3031b7e112c8bd62789
samarthBrowserstack <samarth.s@browserstack.com>
2023-03-03 12:31:05 +0530

    Updated amplitude init

--- packages/utils/src/amplitude.js
+++ packages/utils/src/amplitude.js
@@ -1,9 +1,36 @@
 import amplitude from 'amplitude-js';
 
 let initialized = false;
-export const initAmplitude = (key) => {
+export const initAmplitude = (config) => {
   initialized = true;
-  amplitude.getInstance().init(key);
+  amplitude.getInstance().init(config.key);
+
+  if (config?.userData) {
+    try {
+      amplitude.getInstance().setUserId(config.userData.user_id);
+      amplitude.getInstance().setUserProperties(config.userData);
+    } catch (error) {
+      throw new Error(`Amplitude failed to setup user data ${error}`);
+    }
+  }
+
+  if (config?.groupData) {
+    const amplitudeClient = amplitude.getInstance();
+    amplitudeClient.setGroup('Group', `${config.groupData.group_id}`);
+    const identify = new amplitude.Identify();
+    for (
+      let i = 0, keys = Object.keys(config.groupData);
+      i < keys.length;
+      i += 1
+    ) {
+      identify.set(keys[i], config.groupData[keys[i]]);
+    }
+    amplitudeClient.groupIdentify(
+      'Group',
+      `${config.groupData.group_id}`,
+      identify
+    );
+  }
 };
 
 export const LogAmplitudeEvent = (key, data, cb) => {
