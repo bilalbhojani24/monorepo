08f4bf94f821da7b1a997def1ff32b3d8d287677
Swaraj Gandhi <swaraj@browserstack.com>
2023-03-06 18:59:20 +0530

    added welcome page

--- apps/accessibility/src/common/Welcome/index.jsx
+++ apps/accessibility/src/common/Welcome/index.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { MdCheckCircle } from '@browserstack/bifrost';
 
 export default function Welcome() {
   return (
-    <div className="h-screen">
-      <div />
-      <p className="text-base-900 text-base">
-        You have successfully signed in. This window will auto-close in a few
-        seconds.
-      </p>
+    <div className="bg-base-50 flex h-screen w-screen items-center justify-center">
+      <div className="w-1/2 bg-white shadow">
+        <div className="border-base-200 flex items-center justify-center border-b py-6">
+          <img
+            src="https://d2ogrdw2mh0rsl.cloudfront.net/production/images/static/logo.svg"
+            alt="browserstack-logo"
+            className="h-9"
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center pt-12 pb-8">
+          <MdCheckCircle className="text-success-700 mb-4 text-5xl" />
+          <p className="text-base-900 w-96 text-center text-base">
+            You have successfully signed in. This window will auto-close in a
+            few seconds.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
