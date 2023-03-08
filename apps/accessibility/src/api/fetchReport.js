eb7d61fdd4e03856c123661ed2fe3b18b56e71a8
Swaraj Gandhi <swaraj@browserstack.com>
2023-03-08 14:22:17 +0530

    auth change

--- apps/accessibility/src/api/fetchReport.js
+++ apps/accessibility/src/api/fetchReport.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
-export default async function fetchReport(ids) {
-  const response = await axios.get(`/tests/consolidate?ids=${ids}`, {
+export default async function fetchReport({ ids, arList }) {
+  let params = '';
+  if (ids) {
+    params += `ids=${ids}`;
+  }
+  if (arList) {
+    params += `&ar_ids=${arList}`;
+  }
+  const response = await axios.get(`/tests/consolidate?${params}`, {
     headers: {
       Accept: 'application/json'
     }
--- apps/accessibility/src/utils/index.js
+++ apps/accessibility/src/utils/index.js
@@ -37,6 +37,9 @@ export const defaultPath = () => {
 
 export const getCurrentEnv = () => {
   const { href } = window.location;
+  if (href.includes('localhost')) {
+    return ENVS.LOCAL;
+  }
   if (href.includes('bsstag.com')) {
     return ENVS.STAGING;
   }
@@ -49,7 +52,9 @@ export const getCurrentEnv = () => {
 export const getEnvUrl = () => {
   const env = getCurrentEnv();
   let baseURL = 'https://accessibility-beta.browserstack.com/api';
-  if (env === ENVS.STAGING) {
+  if (env === ENVS.LOCAL) {
+    baseURL = 'https://accessibility.bsstag.com/api';
+  } else if (env === ENVS.STAGING) {
     baseURL = 'https://accessibility.bsstag.com/api';
   } else if (env === ENVS.PRODUCTION) {
     baseURL = 'https://accessibility-beta.browserstack.com/api';
@@ -60,7 +65,9 @@ export const getEnvUrl = () => {
 export const getBrowserStackEnvUrl = () => {
   const env = getCurrentEnv();
   let baseURL = 'https://www.browserstack.com/accessibility/api';
-  if (env === ENVS.STAGING) {
+  if (env === ENVS.LOCAL) {
+    baseURL = 'https://devaccessibility.bsstag.com/accessibility/api';
+  } else if (env === ENVS.STAGING) {
     baseURL = 'https://devaccessibility.bsstag.com/accessibility/api';
   } else if (env === ENVS.PRODUCTION) {
     baseURL = 'https://www.browserstack.com/accessibility/api';
