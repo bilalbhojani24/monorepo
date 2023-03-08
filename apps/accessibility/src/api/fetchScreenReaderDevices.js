3a87588db75cc4eca7228f5e9c7f847c432921dd
Swaraj Gandhi <swaraj@browserstack.com>
2023-03-08 16:34:08 +0530

    added change for the screen reader

--- apps/accessibility/src/api/fetchScreenReaderDevices.js
+++ apps/accessibility/src/api/fetchScreenReaderDevices.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import { getBrowserStackEnvUrl } from 'utils';
+import { getEnvUrl } from 'utils';
 
 export default async function fetchScreenReaderDevices() {
-  const baseURL = getBrowserStackEnvUrl();
+  const baseURL = getEnvUrl();
   const response = await axios.get(
     '/screen-readers/supported-combinations',
     baseURL,
