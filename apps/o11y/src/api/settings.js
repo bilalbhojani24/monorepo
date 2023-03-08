908c7514a3a64048da37fb714211cdd92350bd60
Vishal Sanserwal <vishal.san@browserstack.com>
2023-03-08 18:34:25 +0530

    removed commented code

--- apps/o11y/src/api/settings.js
+++ apps/o11y/src/api/settings.js
@@ -14,31 +14,3 @@ export const updateSettingsByKey = async (
     `${versionedBaseRoute()}/projects/${projectNormalisedName}/settings/${key}`,
     payload
   );
-
-// export const getGeneralSettings = async ({ projectNormalisedName }) =>
-//   axios.get(
-//     `${versionedBaseRoute()}/projects/${projectNormalisedName}/settings/general`
-//   );
-
-// export const updateGeneralSettings = async ({
-//   projectNormalisedName,
-//   payload = {}
-// }) =>
-//   axios.put(
-//     `${versionedBaseRoute()}/projects/${projectNormalisedName}/settings/general`,
-//     payload
-//   );
-
-// export const getAutoAnalyserSettings = async ({ projectNormalisedName }) =>
-//   axios.get(
-//     `${versionedBaseRoute()}/projects/${projectNormalisedName}/settings/auto-analyser`
-//   );
-
-// export const updateAutoAnalyserSettings = async ({
-//   projectNormalisedName,
-//   payload = {}
-// }) =>
-//   axios.put(
-//     `${versionedBaseRoute()}/projects/${projectNormalisedName}/settings/auto-analyser`,
-//     payload
-//   );
