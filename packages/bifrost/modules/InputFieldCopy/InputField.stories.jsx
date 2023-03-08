1500738175b845a1902f5643dde65607bdf43b73
Siva K <sivakwalker3@gmail.com>
2023-03-08 11:59:37 +0530

    feat: input field revamp

---
+++ packages/bifrost/modules/InputFieldCopy/InputField.stories.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import { UsersIcon } from '@heroicons/react/20/solid';
+
+import DocPageTemplate from '../../.storybook/DocPageTemplate';
+import Button from '../Button';
+
+import InputField from './index';
+
+const defaultConfig = {
+  title: 'Application/Components/InputFieldCopy',
+  component: InputField,
+  parameters: {
+    docs: {
+      page: () => (
+        <DocPageTemplate
+          importStatement={"import InputField from 'bifrost/InputField'"}
+        />
+      )
+    }
+  },
+  argTypes: {
+    label: {
+      type: { summary: 'STRING', required: false },
+      description: 'Content that goes inside the banner',
+      control: { type: 'text' },
+      defaultValue: 'Label'
+    },
+    autoComplete: {
+      type: { summary: 'STRING', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: 'text' },
+      defaultValue: 'off'
+    },
+    cornerHintText: {
+      type: { summary: 'STRING', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: 'text' },
+      defaultValue: ''
+    },
+    defaultValue: {
+      type: { summary: 'STRING', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: 'text' },
+      defaultValue: undefined
+    },
+    value: {
+      type: { summary: 'STRING', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: 'text' },
+      defaultValue: undefined
+    },
+    description: {
+      type: { summary: 'STRING', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: 'text' },
+      defaultValue: ''
+    },
+    errorText: {
+      type: { summary: 'STRING', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: 'text' },
+      defaultValue: ''
+    },
+    id: {
+      type: { summary: 'STRING', required: true },
+      description: 'ABCDEFGHIJK',
+      control: { type: 'text' },
+      defaultValue: 'test-id'
+    },
+    inputRef: {
+      type: { summary: 'OBJECT', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: null }
+    },
+    startAdornment: {
+      type: { summary: 'NODE', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: null },
+      defaultValue: <UsersIcon className="text-base-400 h-5 w-5" />
+    },
+    endAdornment: {
+      type: { summary: 'NODE', required: false },
+      description: 'ABCDEFGHIJK',
+      //   control: { type: null },
+      defaultValue: <UsersIcon className="text-base-400 h-5 w-5" />
+    },
+    addOnBefore: {
+      type: { summary: 'NODE', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: null },
+      defaultValue: <Button>Before</Button>
+    },
+    addOnAfter: {
+      type: { summary: 'NODE', required: false },
+      description: 'ABCDEFGHIJK',
+      //   control: { type: null },
+      defaultValue: <Button>After</Button>
+    },
+    placeholder: {
+      type: { summary: 'STRING', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: 'text' },
+      defaultValue: 'Placeholder text'
+    },
+    readonly: {
+      type: { summary: 'STRING', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: 'boolean' },
+      defaultValue: false
+    },
+    type: {
+      type: { summary: 'STRING', required: false },
+      description: 'ABCDEFGHIJK',
+      control: { type: 'text' },
+      defaultValue: 'text'
+    }
+  },
+  controls: {}
+};
+const Template = (args) => <InputField {...args} />;
+const Primary = Template.bind({});
+Primary.parameters = {
+  controls: {}
+};
+
+export default defaultConfig;
+export { Primary };
---
+++ packages/bifrost/modules/InputFieldCopy/index.jsx
@@ -0,0 +1,177 @@
+import React, { forwardRef } from 'react';
+import { twClassNames } from '@browserstack/utils';
+import PropTypes from 'prop-types';
+
+import { ExclamationCircleIcon } from '../Icon';
+
+import './styles.scss';
+
+const InputField = forwardRef(
+  (
+    {
+      autoComplete,
+      cornerHintText,
+      defaultValue,
+      description,
+      disabled,
+      errorText,
+      id,
+      inputRef,
+      label,
+      onBlur,
+      onChange,
+      onKeyDown,
+      onFocus,
+      placeholder,
+      readonly,
+      type,
+      value,
+      wrapperClassName,
+      startAdornment,
+      endAdornment,
+      addOnBefore,
+      addOnAfter
+    },
+    ref
+  ) => (
+    <div>
+      {(label || cornerHintText) && (
+        <div className="mb-1 flex justify-between" role="contentinfo">
+          <label
+            htmlFor={id}
+            id={`${id}label-wrap`}
+            className="text-base-700 block text-sm font-medium"
+          >
+            {label}
+          </label>
+          {cornerHintText && (
+            <span className="text-base-500 text-sm">{cornerHintText}</span>
+          )}
+        </div>
+      )}
+      <div className={twClassNames('flex w-full shadow-sm')}>
+        <div className="flex items-center overflow-hidden rounded-l-md">
+          {addOnBefore}
+        </div>
+        <div
+          className={twClassNames(
+            'relative w-full overflow-hidden flex border-1 focus-within:outline border border-base-300 focus-within:outline-1 focus-within:border-brand-500 focus-within:outline-brand-500',
+            wrapperClassName,
+            {
+              'border-danger-500 focus-within:border-danger-500 focus-within:outline-danger-500':
+                errorText,
+              'cursor-not-allowed border-base-200 bg-base-50 pointer-events-none':
+                disabled,
+              'bg-base-50 bg-clip-padding border-base-300 focus-within:border-brand-600 focus-within:outline-brand-600':
+                readonly
+            }
+          )}
+        >
+          <div className="flex items-center pl-3">{startAdornment}</div>
+          <input
+            aria-invalid={!!errorText}
+            aria-describedby={id + (errorText ? 'error-wrap' : 'label-wrap')}
+            defaultValue={defaultValue}
+            value={value}
+            disabled={disabled}
+            onChange={onChange}
+            onKeyDown={onKeyDown}
+            type={type}
+            ref={ref || inputRef}
+            name={label}
+            id={id}
+            className={twClassNames(
+              'border-none rounded-none bg-transparent sm:text-sm focus:ring-0 w-full flex-1',
+              {
+                'text-danger-900': errorText,
+                'disabled:text-base-500': disabled,
+                'text-base-900 focus:text-base-700': readonly
+              }
+            )}
+            placeholder={placeholder}
+            readOnly={readonly}
+            onFocus={onFocus}
+            onBlur={onBlur}
+            autoComplete={autoComplete}
+          />
+
+          <div className={twClassNames('flex items-center pr-3 gap-1')}>
+            {endAdornment}
+            {errorText && (
+              <ExclamationCircleIcon
+                className="text-danger-500 h-5 w-5"
+                aria-hidden="true"
+              />
+            )}
+          </div>
+        </div>
+        <div className="flex items-center overflow-hidden rounded-r-md">
+          {addOnAfter}
+        </div>
+      </div>
+
+      {description && (
+        <p className="text-base-500 mt-2 text-sm">{description}</p>
+      )}
+      {errorText && (
+        <p className="text-danger-600 mt-2 text-sm" id={`${id}error-wrap`}>
+          {errorText}
+        </p>
+      )}
+    </div>
+  )
+);
+
+InputField.propTypes = {
+  autoComplete: PropTypes.string,
+  cornerHintText: PropTypes.string,
+  defaultValue: PropTypes.string,
+  description: PropTypes.string,
+  disabled: PropTypes.bool,
+  errorText: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  inputRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) })
+  ]),
+  label: PropTypes.string,
+  startAdornment: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
+  endAdornment: PropTypes.oneOfType([PropTypes.node, PropTypes.string]),
+  addOnBefore: PropTypes.node,
+  addOnAfter: PropTypes.node,
+  onBlur: PropTypes.func,
+  onChange: PropTypes.func,
+  onKeyDown: PropTypes.func,
+  onFocus: PropTypes.func,
+  placeholder: PropTypes.string,
+  readonly: PropTypes.bool,
+  type: PropTypes.string,
+  value: PropTypes.string,
+  wrapperClassName: PropTypes.string
+};
+
+InputField.defaultProps = {
+  autoComplete: 'off',
+  cornerHintText: '',
+  defaultValue: undefined,
+  description: '',
+  disabled: false,
+  errorText: '',
+  inputRef: null,
+  label: '',
+  startAdornment: null,
+  endAdornment: null,
+  addOnBefore: null,
+  addOnAfter: null,
+  onBlur: () => {},
+  onChange: () => {},
+  onKeyDown: () => {},
+  onFocus: () => {},
+  placeholder: '',
+  readonly: false,
+  type: 'text',
+  value: undefined,
+  wrapperClassName: ''
+};
+
+export default InputField;
