ba6c561a3f19aca82b9935d8b7805cc6d33a4102
rajeev-bstack <rajeev@browserstack.com>
2023-03-08 10:05:46 +0530

    INTG - 21 | @rajeev-bstack | Separate out Draggable and Resizable

--- packages/integrations/src/features/BasicWidget/components/DraggableResizable.jsx
+++ packages/integrations/src/features/BasicWidget/components/DraggableResizable.jsx
@@ -50,7 +50,7 @@ const DraggableResizable = ({ children, childRef }) => {
           className="border-base-200 relative flex flex-col overflow-hidden rounded-md border border-solid"
           handle={(__resizeHandleAxis, ref) => (
             <div
-              className="bg-base-200 relative bottom-0 left-1/2 mb-1 inline-block h-1 w-20 -translate-x-1/2 rounded-3xl hover:cursor-s-resize"
+              className="bg-base-200 relative bottom-0 left-1/2 mb-1 h-1 w-20 -translate-x-1/2 rounded-3xl hover:cursor-s-resize"
               ref={ref}
             />
           )}
