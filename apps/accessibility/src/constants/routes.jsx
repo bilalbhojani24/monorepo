75620d75bd7e12118b24cd3422ec02a2b732ea60
Kaustubh Saxena <kaustubh.s@browserstack.com>
2023-03-14 14:24:33 +0530

    Added layout to welcome route

--- apps/accessibility/src/constants/routes.jsx
+++ apps/accessibility/src/constants/routes.jsx
@@ -24,7 +24,7 @@ export const APP_ROUTES = [
   {
     path: ROUTES.welcome,
     isProtected: true,
-    component: <Welcome />
+    component: Layout(<Welcome />)
   },
   {
     path: ROUTES.reports,
