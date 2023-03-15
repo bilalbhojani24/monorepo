cb0419db6aaaaa93f26b1501672c63d911dac162
Vishal Sanserwal <vishal.san@browserstack.com>
2023-03-15 20:04:38 +0530

    added withcredentials fix

--- apps/o11y/src/constants/stageConfigMapping.js
+++ apps/o11y/src/constants/stageConfigMapping.js
@@ -1,19 +1,22 @@
 const STAGING_CONFIG = (envName) => ({
   cookiePrefix: `${envName}__`,
   signInUrl: `https://${envName}.bsstag.com/users/sign_in`,
-  apiUrl: 'https://devtestops-api.bsstag.com'
+  apiUrl: 'https://devtestops-api.bsstag.com',
+  withCredentials: true
 });
 
 export default {
   local: {
     cookiePrefix: 'development__',
     signInUrl: 'https://local.bsstag.com/users/sign_in',
-    apiUrl: 'https://localhost:8082/testops'
+    apiUrl: 'https://localhost:8082/testops',
+    withCredentials: false
   },
   'local-staging': {
     cookiePrefix: 'development__',
     signInUrl: 'https://local.bsstag.com/users/sign_in',
-    apiUrl: STAGING_CONFIG('').apiUrl
+    apiUrl: STAGING_CONFIG('').apiUrl,
+    withCredentials: true
   },
   staging: {
     ...STAGING_CONFIG('devtestops')
@@ -27,11 +30,13 @@ export default {
   preprod: {
     cookiePrefix: 'preprod__',
     signInUrl: 'https://preprod.bsstag.com/users/sign_in',
-    apiUrl: 'https://api-observability-preprod.bsstag.com'
+    apiUrl: 'https://api-observability-preprod.bsstag.com',
+    withCredentials: true
   },
   production: {
     cookiePrefix: '',
     signInUrl: 'https://browserstack.com/users/sign_in',
-    apiUrl: 'https://api-observability.browserstack.com'
+    apiUrl: 'https://api-observability.browserstack.com',
+    withCredentials: true
   }
 };
