6db2cd5383eb29d2ee97b0af22b54fdc46733eb0
Kaustubh Saxena <kaustubh.s@browserstack.com>
2023-03-17 11:43:37 +0530

    added strict mode again

--- apps/accessibility/src/main.jsx
+++ apps/accessibility/src/main.jsx
@@ -11,11 +11,11 @@ import store from './store';
 import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <Layout />
       </BrowserRouter>
     </Provider>
-  // </React.StrictMode>
+  </React.StrictMode>
 );
