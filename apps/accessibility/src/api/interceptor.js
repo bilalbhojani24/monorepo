01450159706d35156a071e00a60c5a4dbefbc84f
Mridul Kumar <mridul.k@browserstack.com>
2023-03-20 16:57:03 +0530

    reset data upon unmount

--- apps/accessibility/src/api/interceptor.js
+++ apps/accessibility/src/api/interceptor.js
@@ -3,10 +3,10 @@ import { getConfigByKey } from 'utils';
 
 axios.defaults.baseURL =
   getConfigByKey('VITE_APP_BACKEND_URL') ||
-  // 'https://accessibility.bsstag.com/api' ||
+  'https://accessibility.bsstag.com/api' ||
   `${window.location.origin}/api`;
 
 axios.interceptors.request.use((config) => ({
-  ...config,
-  withCredentials: true
+  ...config
+  // withCredentials: true
 }));
