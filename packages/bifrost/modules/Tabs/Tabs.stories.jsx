4992aa6b12454f80af7639dd902b01b94b0cada9
f1uk3r <agarwal.umang4321@gmail.com>
2023-03-21 10:40:11 +0530

    icon import updated

--- packages/bifrost/modules/Tabs/Tabs.stories.jsx
+++ packages/bifrost/modules/Tabs/Tabs.stories.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
+
+import DocPageTemplate from '../../.storybook/DocPageTemplate';
 import {
   BuildingOfficeIcon,
   CreditCardIcon,
   UserIcon,
   UsersIcon
-} from '@heroicons/react/20/solid';
-
-import DocPageTemplate from '../../.storybook/DocPageTemplate';
+} from '../Icon';
 
 import { TAB_SHAPE } from './const/tabsConstants';
 import Tabs from './index';
