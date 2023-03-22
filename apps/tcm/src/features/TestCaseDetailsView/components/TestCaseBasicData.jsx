fb98678b8cdc1e248f1b79a5bf8835815d69dddd
ribinBS <ribin@browserstack.com>
2023-03-22 19:48:54 +0530

    capitalise fix

--- apps/tcm/src/features/TestCaseDetailsView/components/TestCaseBasicData.jsx
+++ apps/tcm/src/features/TestCaseDetailsView/components/TestCaseBasicData.jsx
@@ -200,6 +200,7 @@ const TestCaseBasicData = ({ isFromTestRun }) => {
           </div>
         </div>
         <DetailsSnippet
+          dontCapitalize
           title="Attachments"
           value={
             testCaseDetails?.attachments?.length ? (
