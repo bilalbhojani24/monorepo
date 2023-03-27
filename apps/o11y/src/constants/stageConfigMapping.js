4cc6b4c82c5060c91e75cdac0a2a21f1c4b4d99e
Vishal Sanserwal <vishal.san@browserstack.com>
2023-03-27 19:51:08 +0530

    added fixes for QA issues raised

--- apps/o11y/src/constants/stageConfigMapping.js
+++ apps/o11y/src/constants/stageConfigMapping.js
@@ -3,6 +3,7 @@ const STAGING_CONFIG = (envName) => ({
   cookiePrefix: `${envName}${COOKIE_SEPARATOR}`,
   signInUrl: `https://${envName}.bsstag.com/users/sign_in`,
   apiUrl: 'https://devtestops-api.bsstag.com',
+  baseDocUrl: 'https://devtestops.bsstag.com',
   withCredentials: true
 });
 
@@ -11,11 +12,13 @@ export default {
     cookiePrefix: `development${COOKIE_SEPARATOR}`,
     signInUrl: 'https://local.bsstag.com/users/sign_in',
     apiUrl: 'https://localhost:8082/testops',
+    baseDocUrl: STAGING_CONFIG('').baseDocUrl,
     withCredentials: false
   },
   'local-staging': {
     cookiePrefix: `development${COOKIE_SEPARATOR}`,
     signInUrl: 'https://local.bsstag.com/users/sign_in',
+    baseDocUrl: STAGING_CONFIG('').baseDocUrl,
     apiUrl: STAGING_CONFIG('').apiUrl,
     withCredentials: true
   },
@@ -32,12 +35,14 @@ export default {
     cookiePrefix: `preprod${COOKIE_SEPARATOR}`,
     signInUrl: 'https://preprod.bsstag.com/users/sign_in',
     apiUrl: 'https://api-observability-preprod.bsstag.com',
+    baseDocUrl: 'https://preprod.bsstag.com',
     withCredentials: true
   },
   production: {
     cookiePrefix: '',
     signInUrl: 'https://browserstack.com/users/sign_in',
     apiUrl: 'https://api-observability.browserstack.com',
+    baseDocUrl: 'https://browserstack.com',
     withCredentials: true,
     enableAnalytics: true
   }
