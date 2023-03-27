48749fca5f9a254dae648231fbbb83356cc5b433
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-03-27 14:10:20 +0530

    padding added to header top

--- packages/bifrost/modules/AccordionPanel/index.jsx
+++ packages/bifrost/modules/AccordionPanel/index.jsx
@@ -10,7 +10,7 @@ const AccordionPanel = ({ children }) => (
     leave="transition duration-200 ease-out"
     leaveFrom="transform scale-100 opacity-100"
     leaveTo="transform scale-95 opacity-0"
-    className="pb-4 pl-6"
+    className="pt-2 pb-4 pl-6"
   >
     <Disclosure.Panel>{children}</Disclosure.Panel>{' '}
   </Transition>
