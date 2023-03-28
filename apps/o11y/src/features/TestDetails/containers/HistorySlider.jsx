0e6a7176b875172023bf07f56a8b8d80e95da076
Siva K <sivakwalker3@gmail.com>
2023-03-28 09:28:26 +0530

    updated history slider to tabs

--- apps/o11y/src/features/TestDetails/containers/HistorySlider.jsx
+++ apps/o11y/src/features/TestDetails/containers/HistorySlider.jsx
@@ -1,42 +1,12 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { twClassNames } from '@browserstack/utils';
+import { O11yTabs } from 'common/bifrostProxy';
 import StatusIcon from 'common/StatusIcon';
-import PropTypes from 'prop-types';
+import isEmpty from 'lodash/isEmpty';
 
 import { getShowTestDetailsFor, getTestMeta } from '../slices/selectors';
 import { setShowTestDetailsFor } from '../slices/uiSlice';
-
-const HistoryItem = ({ data, isActive, onClick }) => (
-  <button
-    className={twClassNames('flex items-center gap-2 p-1 pb-4 ', {
-      'border-b-2 border-brand-500': isActive
-    })}
-    onClick={() => {
-      onClick(data);
-    }}
-    type="button"
-  >
-    <StatusIcon
-      status={data?.status}
-      {...(isActive ? {} : { noColor: true })}
-    />
-    <p
-      className={twClassNames('text-base-500 text-sm font-medium leading-5', {
-        'text-brand-500': isActive
-      })}
-    >
-      {data.serialId}
-    </p>
-  </button>
-);
-
-HistoryItem.propTypes = {
-  data: PropTypes.objectOf(PropTypes.any).isRequired,
-  isActive: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired
-};
 
 const HistorySlider = () => {
   const dispatch = useDispatch();
@@ -44,33 +14,75 @@ const HistorySlider = () => {
   const testMeta = useSelector(getTestMeta);
   const testRunId = useSelector(getShowTestDetailsFor);
 
-  const scrollerRef = useRef(null);
+  const [activeTab, setActiveTab] = useState({
+    idx: 0,
+    value: null,
+    name: null
+  });
 
-  const handleActiveSlide = useCallback(
-    (data) => {
-      dispatch(setShowTestDetailsFor(data.testRunId));
+  const tabs = useMemo(() => {
+    if (!isEmpty(testMeta.data?.history)) {
+      return testMeta.data?.history.map((testHistory) => ({
+        name: testHistory.serialId,
+        value: testHistory.testRunId,
+        icon: () => (
+          <div className="mr-2 flex h-full items-center">
+            <StatusIcon
+              status={testHistory?.status}
+              {...(activeTab.value === testHistory.testRunId
+                ? {}
+                : { noColor: true })}
+            />
+          </div>
+        )
+      }));
+    }
+    return [];
+  }, [activeTab.value, testMeta.data?.history]);
+
+  useEffect(() => {
+    const allTestHistory = testMeta.data?.history;
+    if (!isEmpty(allTestHistory)) {
+      const activeTestHistoryIdx = allTestHistory.findIndex(
+        (testHistory) => testHistory.testRunId === testRunId
+      );
+
+      if (activeTestHistoryIdx !== -1) {
+        const activeTestHistory = allTestHistory[activeTestHistoryIdx];
+        setActiveTab({
+          idx: activeTestHistoryIdx,
+          value: activeTestHistory.testRunId,
+          name: activeTestHistory.serialId
+        });
+      }
+    }
+  }, [testMeta.data?.history, testRunId]);
+
+  const onTabChange = useCallback(
+    (tabInfo) => {
+      let activeIndex = tabs.findIndex((item) => item.value === tabInfo.value);
+      activeIndex = activeIndex === -1 ? 0 : activeIndex;
+
+      setActiveTab({
+        idx: activeIndex,
+        value: tabInfo.value,
+        name: tabInfo.name
+      });
+      dispatch(setShowTestDetailsFor(tabInfo.value));
       const searchParams = new URLSearchParams(window?.location?.search);
-      searchParams.set('details', data.testRunId);
+      searchParams.set('details', tabInfo.value);
       navigate({ search: searchParams.toString() });
     },
-    [dispatch, navigate]
+    [dispatch, navigate, tabs]
   );
 
   return (
     <div className="mb-4 px-6">
-      <div
-        className="border-base-300 flex flex-nowrap gap-8 overflow-x-auto border-b"
-        ref={scrollerRef}
-      >
-        {testMeta.data?.history?.map((testHistory) => (
-          <HistoryItem
-            key={testHistory.testRunId}
-            data={testHistory}
-            isActive={testRunId === testHistory.testRunId}
-            onClick={handleActiveSlide}
-          />
-        ))}
-      </div>
+      <O11yTabs
+        defaultIndex={activeTab.idx}
+        tabsArray={tabs}
+        onTabChange={onTabChange}
+      />
     </div>
   );
 };
