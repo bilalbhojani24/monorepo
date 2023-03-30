626508570bcca99d505d5562ebf94755f102e009
arsalanPro <arsalan.k@browserstack.com>
2023-03-30 17:28:58 +0530

    minor fixes in text

--- apps/tcm/src/features/importCSVFlow/components/useTextTransformer.js
+++ apps/tcm/src/features/importCSVFlow/components/useTextTransformer.js
@@ -1,13 +1,13 @@
 import { useEffect, useState } from 'react';
 
 const useTextTransformer = ({ textRef, text }) => {
-  const [transformedText, setTransformedText] = useState(text);
+  const [transformedText, setTransformedText] = useState(null);
 
   useEffect(() => {
     if (textRef.current?.clientWidth < textRef.current?.scrollWidth) {
       const textArray = text.split('/');
       setTransformedText(`/../${textArray[textArray.length - 2]}/`);
-    } else setTransformedText(text);
+    } else setTransformedText(null);
   }, [textRef, text]);
 
   return { transformedText };
