0918ad36a53983cfb89b7362efc1e193e097dfe8
Siva K <sivakwalker3@gmail.com>
2023-04-03 12:28:46 +0530

    mono font added for o11y

--- apps/o11y/tailwind.config.js
+++ apps/o11y/tailwind.config.js
@@ -2,5 +2,24 @@ const globalConfigs = require('@browserstack/tailwind-config');
 
 module.exports = {
   ...globalConfigs.globalTailwindConfig,
-  content: ['src/**/*.{js,jsx}', '../../packages/bifrost/modules/**/*.{js,jsx}']
+  content: [
+    'src/**/*.{js,jsx}',
+    '../../packages/bifrost/modules/**/*.{js,jsx}'
+  ],
+  theme: {
+    ...globalConfigs.globalTailwindConfig.theme,
+    fontFamily: {
+      ...globalConfigs.globalTailwindConfig.theme.fontFamily,
+      mono: [
+        'ui-monospace',
+        'SFMono-Regular',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        'Liberation Mono',
+        'Courier New',
+        'monospace'
+      ]
+    }
+  }
 };
