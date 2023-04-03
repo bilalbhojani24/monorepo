ef50d9c1d0406bd0c5636eb4625c9b9bd491369e
Ashu <ashutosh.d@browserstack.com>
2023-04-04 01:26:22 +0530

    improving auto update logic

--- apps/mcspt/src/mainThreadFunctions/autoUpdateOps.js
+++ apps/mcspt/src/mainThreadFunctions/autoUpdateOps.js
@@ -1,15 +1,11 @@
-const { app } = require('electron');
-
 const updateApp = require('update-electron-app');
 
 export const initializeAutoUpdate = () => {
   if (!IS_DEV) {
-    app.on('ready', () => {
-      updateApp({
-        updateInterval: '1 hour',
-        logger: require('electron-log'),
-        notifyUser: true
-      });
+    updateApp({
+      updateInterval: '1 hour',
+      logger: require('electron-log'),
+      notifyUser: true
     });
   }
 };
