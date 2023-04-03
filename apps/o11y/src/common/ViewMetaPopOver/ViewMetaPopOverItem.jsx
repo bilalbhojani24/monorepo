c9ab662d02fb6caa3bb3ae0a5ca7546bd6a8873f
Vishal Sanserwal <vishal.san@browserstack.com>
2023-04-03 17:12:09 +0530

    spacing reduced

--- apps/o11y/src/common/ViewMetaPopOver/ViewMetaPopOverItem.jsx
+++ apps/o11y/src/common/ViewMetaPopOver/ViewMetaPopOverItem.jsx
@@ -14,7 +14,7 @@ function ViewMetaPopOverItem({ title, text, showCopy, textToCopy, link }) {
     }, 2000);
   };
   return (
-    <div className="flex flex-col py-4 first:pt-0 last:pb-0">
+    <div className="flex flex-col py-2 first:pt-0 last:pb-0">
       <p className="text-base-500 text-sm font-medium">{title}</p>
       <div className="mt-1 flex items-center justify-between gap-4">
         <p className="truncate text-sm font-medium">{text}</p>
