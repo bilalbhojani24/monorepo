68a800ac4550dd06ab081dcc240058e6bf54f275
Mridul Kumar <mridul.k@browserstack.com>
2023-04-03 18:20:19 +0530

    fix: added spinning loader component

--- apps/accessibility/src/features/SiteScanner/index.jsx
+++ apps/accessibility/src/features/SiteScanner/index.jsx
@@ -45,6 +45,7 @@ import {
   stopRecurringScans
 } from '../../api/siteScannerScanConfigs';
 import Loader from '../../common/Loader/index';
+import SpinningLoader from '../../common/SpinningLoader';
 import { getWcagVersionFromVal } from '../../utils/helper';
 
 import { getScanConfigs } from './slices/dataSlice';
@@ -206,27 +207,7 @@ export default function SiteScanner() {
     if (row.isProcessing && !Object.keys(row.lastScanDetails).length) {
       return (
         <div className="flex items-center">
-          <svg
-            aria-hidden="true"
-            className="mr-2"
-            style={{
-              height: '20px',
-              width: '20px',
-              animation: 'spin 1s linear infinite'
-            }}
-            viewBox="0 0 100 101"
-            fill="#0070F0"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-              fill="rgba(209, 213, 219, 1)"
-            />
-            <path
-              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-              fill="currentFill"
-            />
-          </svg>
+          <SpinningLoader />
           Initializing your scan
         </div>
       );
@@ -406,7 +387,7 @@ export default function SiteScanner() {
   const getColdStart = () =>
     scanConfigStateData.success ? (
       <div className="justify-center" style={{ height: 'calc(100vh - 320px)' }}>
-        <div className="mt-40 mb-5 flex w-full flex-col items-center justify-center">
+        <div className="mb-5 mt-40 flex w-full flex-col items-center justify-center">
           <img src={NewScanLogo} alt="new scan logo" className="mb-5" />
           <div className="mb-5 flex flex-col items-center">
             <span>No scans to show.</span>
@@ -471,7 +452,7 @@ export default function SiteScanner() {
           <div className="block p-6 pt-0">
             <div className="flex justify-between">
               <div className="flex items-center">
-                <div className="mt-1 mr-4 w-64">
+                <div className="mr-4 mt-1 w-64">
                   <InputField
                     onChange={handleSearch}
                     id="search-scan"
@@ -479,7 +460,7 @@ export default function SiteScanner() {
                     leadingIcon={<MdSearch />}
                   />
                 </div>
-                <div className="mt-1 mr-4">
+                <div className="mr-4 mt-1">
                   <Dropdown onClick={handleSearchFilter} id="scanFilter">
                     <div className="flex">
                       <DropdownTrigger wrapperClassName="border-base-300 text-base-700 hover:bg-base-50 focus:ring-offset-base-100 focus:ring-brand-500 inline-flex w-full justify-center rounded-md border bg-white px-4 py-2 text-sm font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2">
@@ -584,27 +565,7 @@ export default function SiteScanner() {
                           Object.keys(row.lastScanDetails).length ? (
                             <div className="mt-2 flex items-center">
                               Scan Ongoing
-                              <svg
-                                aria-hidden="true"
-                                className="ml-2"
-                                style={{
-                                  height: '20px',
-                                  width: '20px',
-                                  animation: 'spin 1s linear infinite'
-                                }}
-                                viewBox="0 0 100 101"
-                                fill="#0070F0"
-                                xmlns="http://www.w3.org/2000/svg"
-                              >
-                                <path
-                                  d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                                  fill="rgba(209, 213, 219, 1)"
-                                />
-                                <path
-                                  d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                                  fill="currentFill"
-                                />
-                              </svg>
+                              <SpinningLoader />
                             </div>
                           ) : null}
                           {!row.isProcessing && row.nextScanDate ? (
@@ -622,7 +583,7 @@ export default function SiteScanner() {
                       <TableCell>
                         {row?.lastScanDetails?.reportSummary ? (
                           <div className="flex">
-                            <div className="w-11 ml-1">
+                            <div className="ml-1 w-11">
                               <Tooltip
                                 theme="dark"
                                 placementSide="bottom"
@@ -642,7 +603,7 @@ export default function SiteScanner() {
                                 </span>
                               </Tooltip>
                             </div>
-                            <div className="w-11 ml-1">
+                            <div className="ml-1 w-11">
                               <Tooltip
                                 theme="dark"
                                 placementSide="bottom"
@@ -662,7 +623,7 @@ export default function SiteScanner() {
                                 </span>
                               </Tooltip>
                             </div>
-                            <div className="w-11 ml-1">
+                            <div className="ml-1 w-11">
                               <Tooltip
                                 theme="dark"
                                 placementSide="bottom"
@@ -736,7 +697,7 @@ export default function SiteScanner() {
             >
               <div className="mb-5 flex w-full flex-col items-center justify-center">
                 <img src={NotFound} alt="No scans found" className="w-80" />
-                <p className="text-base-500 text-sm mt-2">No scans to show</p>
+                <p className="text-base-500 mt-2 text-sm">No scans to show</p>
               </div>
             </div>
           )}
