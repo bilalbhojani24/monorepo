082c9cc14a1e52e82e17b71253a505b28c41426c
samarthBrowserstack <samarth.s@browserstack.com>
2023-04-04 12:32:07 +0530

    Added meta title and changed div to span in metadarta

--- packages/bifrost/modules/Metadata/index.jsx
+++ packages/bifrost/modules/Metadata/index.jsx
@@ -4,31 +4,32 @@ import PropTypes from 'prop-types';
 
 import './styles.scss';
 
-const Metadata = ({ icon, metaDescription, textColorClass }) => (
-  <>
-    <div
-      className={twClassNames(
-        'flex flex-row w-fit gap-1.5 justify-center items-center text-base-300',
-        textColorClass
-      )}
-    >
-      {icon}
-      <span className={twClassNames('font-medium text-sm leading-5')}>
-        {metaDescription}
-      </span>
-    </div>
-  </>
+const Metadata = ({ icon, metaDescription, textColorClass, metaTitle }) => (
+  <span
+    title={metaTitle}
+    className={twClassNames(
+      'flex flex-row w-fit gap-1.5 justify-center items-center text-base-300',
+      textColorClass
+    )}
+  >
+    {icon}
+    <span className={twClassNames('font-medium text-sm leading-5')}>
+      {metaDescription}
+    </span>
+  </span>
 );
 
 Metadata.propTypes = {
   icon: PropTypes.node,
   metaDescription: PropTypes.string,
-  textColorClass: PropTypes.string
+  textColorClass: PropTypes.string,
+  metaTitle: PropTypes.string
 };
 Metadata.defaultProps = {
   icon: null,
   metaDescription: '',
-  textColorClass: ''
+  textColorClass: '',
+  metaTitle: ''
 };
 
 export default Metadata;
