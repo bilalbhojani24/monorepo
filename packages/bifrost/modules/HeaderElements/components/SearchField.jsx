4b0ffd32995bfd5f78cc5ca166cf03ee172da77b
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-04-05 17:51:27 +0530

    focus issue on cross icon trpped resolved

--- packages/bifrost/modules/HeaderElements/components/SearchField.jsx
+++ packages/bifrost/modules/HeaderElements/components/SearchField.jsx
@@ -91,7 +91,10 @@ const SearchField = React.forwardRef(
                     colors="white"
                     isIconOnlyButton
                     icon={<MdClose className="text-base-500 h-5 w-5" />}
-                    onClick={() => setSearchValue('')}
+                    onClick={() => {
+                      setSearchValue('');
+                      ref.current.focus();
+                    }}
                     wrapperClassName="p-0"
                   />
                   <Button
