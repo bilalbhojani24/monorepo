36a08a644a0445fed52798ebbed22c83a9d1979a
Vishal Sanserwal <vishal.san@browserstack.com>
2023-04-05 16:22:49 +0530

    minor updates

--- apps/o11y/src/utils/interceptors.js
+++ apps/o11y/src/utils/interceptors.js
@@ -1,35 +1,45 @@
 import axios from 'axios';
 import { getEnvConfig } from 'utils/common';
 
+const getMockerConfig = (config) => {
+  const updatedConfig = {};
+  if (typeof config.data === 'object') {
+    // To submit form data
+    if (config.headers['Content-Type'] === 'multipart/form-data') {
+      config.data?.append('authenticity_token', new Date());
+    } else {
+      updatedConfig.data = {
+        ...config.data,
+        authenticity_token: new Date()
+      };
+    }
+  } else {
+    updatedConfig.data = `${
+      config.data ? `${config.data}&` : ''
+    }authenticity_token=${encodeURIComponent(new Date())}`;
+  }
+  return updatedConfig;
+};
+
 axios.interceptors.request.use((config) => {
-  const updatedConfig = config;
-  updatedConfig.baseURL = getEnvConfig().apiUrl;
-  updatedConfig.withCredentials = getEnvConfig().withCredentials;
+  let updatedConfig = config;
+  const stageConfig = getEnvConfig();
+  updatedConfig.baseURL = stageConfig.apiUrl;
+  updatedConfig.withCredentials = stageConfig.withCredentials;
   updatedConfig.headers = updatedConfig.headers || {};
-  updatedConfig.headers['x-cookie-prefix'] = getEnvConfig().cookiePrefix;
+  updatedConfig.headers['x-cookie-prefix'] = stageConfig.cookiePrefix;
 
   // for use in local api-mocker only
   if (
-    !getEnvConfig().withCredentials &&
+    !stageConfig.isMocker &&
     (config.method === 'post' ||
       config.method === 'put' ||
       config.method === 'patch')
   ) {
-    if (typeof config.data === 'object') {
-      // To submit form data
-      if (config.headers['Content-Type'] === 'multipart/form-data') {
-        config.data?.append('authenticity_token', new Date());
-      } else {
-        updatedConfig.data = {
-          ...config.data,
-          authenticity_token: new Date()
-        };
-      }
-    } else {
-      updatedConfig.data = `${
-        config.data ? `${config.data}&` : ''
-      }authenticity_token=${encodeURIComponent(new Date())}`;
-    }
+    updatedConfig = {
+      ...updatedConfig,
+      ...getMockerConfig(config)
+    };
   }
   return updatedConfig;
 });
