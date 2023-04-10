15eef31a5e20f210a8799d3f30aac2c698267b50
Vishal Sanserwal <vishal.san@browserstack.com>
2023-04-10 15:43:44 +0530

    to test on preprod

--- packages/integrations/src/features/BasicWidget/components/WidgetHeader.jsx
+++ packages/integrations/src/features/BasicWidget/components/WidgetHeader.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Bars2Icon, Button, MdClear } from '@browserstack/bifrost';
+import { Button, MdClear } from '@browserstack/bifrost';
 import PropTypes from 'prop-types';
 
 const WidgetHeader = ({ handleClose }) => (
   <div className="bg-base-50 flex items-center justify-between rounded-t-md py-1 px-4 ">
     <Button
-      icon={<Bars2Icon />}
+      // icon={<Bars2Icon />}
       wrapperClassName="drag-handle w-4 h-4 border-0 text-base-400 p-0 bg-inherit hover:bg-inherit focus:ring-0 focus:ring-offset-0 focus:ring-offset-0cursor-grab"
     />
     <Button
