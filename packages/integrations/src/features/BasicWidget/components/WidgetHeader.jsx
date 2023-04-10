1f177494ddad3bd299079dd31bacbb62ce7a9a76
rajeev-bstack <rajeev@browserstack.com>
2023-04-10 16:16:24 +0530

    INTG - 64 | @rajeev-bstack | Replace Bars2Icon with MdDragHandle

--- packages/integrations/src/features/BasicWidget/components/WidgetHeader.jsx
+++ packages/integrations/src/features/BasicWidget/components/WidgetHeader.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Bars2Icon, Button, MdClear } from '@browserstack/bifrost';
+import { Button, MdClear, MdDragHandle } from '@browserstack/bifrost';
 import PropTypes from 'prop-types';
 
 const WidgetHeader = ({ handleClose }) => (
   <div className="bg-base-50 flex items-center justify-between rounded-t-md py-1 px-4 ">
     <Button
-      icon={<Bars2Icon />}
-      wrapperClassName="drag-handle w-4 h-4 border-0 text-base-400 p-0 bg-inherit hover:bg-inherit focus:ring-0 focus:ring-offset-0 focus:ring-offset-0cursor-grab"
+      icon={<MdDragHandle className="h-6 w-6" />}
+      wrapperClassName="drag-handle w-6 h-6 border-0 text-base-400 p-0 bg-inherit hover:bg-inherit focus:ring-0 focus:ring-offset-0 focus:ring-offset-0cursor-grab"
     />
     <Button
       wrapperClassName="hover:bg-inherit border-0 shadow-none bg-inherit focus:ring-0 focus:ring-offset-0 px-0"
