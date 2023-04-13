c3c2c2a2b90a81fc75b9c989dc6f492117b57864
Vishal Sanserwal <vishal.san@browserstack.com>
2023-04-13 19:43:10 +0530

    sanitized search string

--- apps/o11y/src/utils/interceptors.js
+++ apps/o11y/src/utils/interceptors.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { getEnvConfig } from 'utils/common';
 
+import { o11yNotify } from './notification';
+
 axios.interceptors.request.use((config) => {
   const updatedConfig = config;
   updatedConfig.baseURL = getEnvConfig().apiUrl;
@@ -9,3 +11,19 @@ axios.interceptors.request.use((config) => {
   updatedConfig.headers['x-cookie-prefix'] = getEnvConfig().cookiePrefix;
   return updatedConfig;
 });
+
+axios.interceptors.response.use(
+  (res) => Promise.resolve(res),
+  (res) => {
+    if (res?.response?.status === 500) {
+      // if server error, show toast
+      o11yNotify({
+        title: 'Error occurred',
+        description:
+          'Some technical error occurred. Please try again. If this issue persists, contact Support.',
+        type: 'error'
+      });
+    }
+    return Promise.reject(res);
+  }
+);
