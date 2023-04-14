477252d3954af8979f9b8ab95eecfde2d925643a
f1uk3r <agarwal.umang4321@gmail.com>
2023-04-14 11:05:51 +0530

    accessibility fix

--- packages/bifrost/modules/ModalBody/index.jsx
+++ packages/bifrost/modules/ModalBody/index.jsx
@@ -3,7 +3,13 @@ import { twClassNames } from '@browserstack/utils';
 import PropTypes from 'prop-types';
 
 const ModalBody = ({ children, className }) => (
-  <div className={twClassNames('px-6 flex-1 overflow-y-scroll', className)}>
+  <div
+    className={twClassNames('px-6 flex-1 overflow-y-scroll', className)}
+    tabIndex={0}
+    role="scrollbar"
+    aria-valuenow={0}
+    aria-controls=""
+  >
     {children}
   </div>
 );
