7c1b89a15d2defd2cefe1f7a6cd8eff781b2a1cc
samarthBrowserstack <samarth.s@browserstack.com>
2023-04-14 11:07:13 +0530

    Fix a11y for Meatball dropdown trigger

--- packages/bifrost/modules/DropdownTrigger/index.jsx
+++ packages/bifrost/modules/DropdownTrigger/index.jsx
@@ -7,6 +7,7 @@ import PropTypes from 'prop-types';
 const DropdownTrigger = ({ children, onClick, wrapperClassName }) => (
   <Popover.Trigger asChild>
     <Menu.Button
+      aria-label="Dropdown trigger button"
       className={twClassNames(
         'border-base-300 text-base-700 hover:bg-base-50 focus:ring-offset-base-100 focus:ring-brand-500 inline-flex w-full justify-center rounded-md border bg-white px-4 py-2 text-sm font-medium shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2',
         wrapperClassName
