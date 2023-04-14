69df042e1ecd95feb8587fd2e1514c120ea3512d
Vishal Sanserwal <vishal.san@browserstack.com>
2023-04-14 23:08:45 +0530

    excluded credentials

--- apps/o11y/src/utils/interceptors.js
+++ apps/o11y/src/utils/interceptors.js
@@ -1,11 +1,50 @@
 import axios from 'axios';
 import { getEnvConfig } from 'utils/common';
 
+const getMockerConfig = (config) => {
+  const updatedConfig = {};
+  if (typeof config.data === 'object') {
+    // To submit form data
+    if (config.headers['Content-Type'] === 'multipart/form-data') {
+      config.data?.append('authenticity_token', new Date());
+    } else {
+      updatedConfig.data = {
+        ...config.data,
+        authenticity_token: new Date()
+      };
+    }
+  } else {
+    updatedConfig.data = `${
+      config.data ? `${config.data}&` : ''
+    }authenticity_token=${encodeURIComponent(new Date())}`;
+  }
+  return updatedConfig;
+};
+
+export const excludeCredentials = (url) => !!url.includes('https://eds');
+
+const stageConfig = getEnvConfig();
+
 axios.interceptors.request.use((config) => {
-  const updatedConfig = config;
-  updatedConfig.baseURL = getEnvConfig().apiUrl;
-  updatedConfig.withCredentials = getEnvConfig().withCredentials;
+  let updatedConfig = config;
+  updatedConfig.baseURL = stageConfig.apiUrl;
+  updatedConfig.withCredentials = excludeCredentials(config.url)
+    ? false
+    : stageConfig.withCredentials;
   updatedConfig.headers = updatedConfig.headers || {};
-  updatedConfig.headers['x-cookie-prefix'] = getEnvConfig().cookiePrefix;
+  updatedConfig.headers['x-cookie-prefix'] = stageConfig.cookiePrefix;
+
+  // for use in local api-mocker only
+  if (
+    !stageConfig.isMocker &&
+    (config.method === 'post' ||
+      config.method === 'put' ||
+      config.method === 'patch')
+  ) {
+    updatedConfig = {
+      ...updatedConfig,
+      ...getMockerConfig(config)
+    };
+  }
   return updatedConfig;
 });
