369f13130ffc214d7d6ca271ac9191b57a75e246
Vishal Sanserwal <vishal.san@browserstack.com>
2023-04-14 19:09:48 +0530

    colors updated

--- apps/o11y/src/features/TestList/components/TestlistTimeline.jsx
+++ apps/o11y/src/features/TestList/components/TestlistTimeline.jsx
@@ -16,15 +16,15 @@ function TestlistTimeline({ details }) {
   const getClassesByStatus = (status) => {
     switch (status) {
       case TEST_STATUS.FAIL:
-        return 'bg-danger-400 border-danger-300';
+        return 'bg-danger-500 border-danger-500';
       case TEST_STATUS.PASS:
-        return 'bg-success-400 border-success-300';
+        return 'bg-success-500 border-success-500';
       case TEST_STATUS.SKIPPED:
-        return 'bg-base-400 border-base-400';
+        return 'bg-base-500 border-base-500';
       case TEST_STATUS.TIMEOUT:
-        return 'bg-attention-500 border-attention-400';
+        return 'bg-attention-500 border-attention-500';
       default:
-        return 'bg-base-400 border-base-400';
+        return 'bg-base-500 border-base-500';
     }
   };
 
@@ -74,7 +74,7 @@ function TestlistTimeline({ details }) {
               )}
             />
           </div>
-          <div className="bg-base-300 h-0.5 w-2 shrink-0 rounded-full" />
+          <div className="bg-base-400 h-0.5 w-2 shrink-0 rounded-full" />
         </O11yTooltip>
       ))}
     </div>
