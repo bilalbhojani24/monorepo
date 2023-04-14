575823345f822be3aa1935a2b3afcbc5305c415f
Dhruv Rana <22201670+drjaat@users.noreply.github.com>
2023-04-14 11:15:18 +0530

    added space

--- apps/o11y/src/features/TestingTrends/components/CardHeader.jsx
+++ apps/o11y/src/features/TestingTrends/components/CardHeader.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 export default function CardHeader({ title }) {
   return (
-    <div className="flex justify-between px-6 pt-3">
+    <div className="flex justify-between px-6 pt-3 ">
       <p className="text-lg font-semibold">{title}</p>
       <Button
         colors="white"
