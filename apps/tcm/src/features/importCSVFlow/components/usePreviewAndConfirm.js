15134420a4a74e930525503323162ad6c5026e03
ribinBS <ribin@browserstack.com>
2023-04-14 13:28:20 +0530

    progress percetage update

--- apps/tcm/src/features/importCSVFlow/components/usePreviewAndConfirm.js
+++ apps/tcm/src/features/importCSVFlow/components/usePreviewAndConfirm.js
@@ -64,7 +64,7 @@ const usePreviewAndConfirm = () => {
 
   useEffect(() => {
     if (lastMessage?.data) {
-      const percent = JSON.parse(lastMessage.data)?.message?.percent || 0;
+      const percent = JSON.parse(lastMessage.data)?.message?.percent;
       if (percent) dispatch(updateImportProgress(percent));
     }
   }, [dispatch, lastMessage]);
