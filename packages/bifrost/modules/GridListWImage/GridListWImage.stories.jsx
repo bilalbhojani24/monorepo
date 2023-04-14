c7006d0bdbd0a3991e504848b8028bc0d79be0c6
samarthBrowserstack <samarth.s@browserstack.com>
2023-04-14 11:10:37 +0530

    Fix a11y gridlistwimage story

--- packages/bifrost/modules/GridListWImage/GridListWImage.stories.jsx
+++ packages/bifrost/modules/GridListWImage/GridListWImage.stories.jsx
@@ -60,6 +60,7 @@ export const GridExample = () => (
   <div
     role="list"
     className="grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8"
+    aria-busy="true"
   >
     <GridListWImage
       label="Lorem"
