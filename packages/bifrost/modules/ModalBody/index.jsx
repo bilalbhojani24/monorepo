0b5135d84ece073cff6e67e296a4c4c61b472796
f1uk3r <agarwal.umang4321@gmail.com>
2023-04-17 13:07:13 +0530

    tabindex none

--- packages/bifrost/modules/ModalBody/index.jsx
+++ packages/bifrost/modules/ModalBody/index.jsx
@@ -5,7 +5,6 @@ import PropTypes from 'prop-types';
 const ModalBody = ({ children, className }) => (
   <div
     className={twClassNames('px-6 flex-1 overflow-y-scroll', className)}
-    tabIndex={0}
     role="scrollbar"
     aria-valuenow={0}
     aria-controls=""
