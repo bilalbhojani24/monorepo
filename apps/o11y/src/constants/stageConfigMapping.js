7c0ac136978d43e16deb77558485ea3d321e5709
Vishal Sanserwal <vishal.san@browserstack.com>
2023-04-17 15:42:02 +0530

    signout and in url updated

--- apps/o11y/src/constants/stageConfigMapping.js
+++ apps/o11y/src/constants/stageConfigMapping.js
@@ -1,5 +1,6 @@
 const STAGING_CONFIG = (envName) => ({
-  signInUrl: `https://${envName}.bsstag.com/users/sign_in`,
+  signInUrl: `https://devtestops-api.bsstag.com/api/v1/auth/start-sso`,
+  signOutUrl: `https://${envName}.bsstag.com/users/sign_out`,
   apiUrl: 'https://devtestops-api.bsstag.com',
   baseDocUrl: 'https://devtestops.bsstag.com',
   withCredentials: true
@@ -8,6 +9,7 @@ const STAGING_CONFIG = (envName) => ({
 export default {
   local: {
     signInUrl: `${STAGING_CONFIG('').apiUrl}/api/v1/auth/start-sso`,
+    signOutUrl: STAGING_CONFIG('devtestops').signOutUrl,
     apiUrl: 'https://localhost:8082/testops',
     baseDocUrl: STAGING_CONFIG('').baseDocUrl,
     withCredentials: false,
@@ -15,6 +17,7 @@ export default {
   },
   'local-staging': {
     signInUrl: `${STAGING_CONFIG('').apiUrl}/api/v1/auth/start-sso`,
+    signOutUrl: STAGING_CONFIG('devtestops').signOutUrl,
     baseDocUrl: STAGING_CONFIG('').baseDocUrl,
     apiUrl: STAGING_CONFIG('').apiUrl,
     withCredentials: true
@@ -31,6 +34,7 @@ export default {
   preprod: {
     signInUrl:
       'https://api-observability-preprod.bsstag.com/api/v1/auth/start-sso',
+    signOutUrl: 'https://preprod.bsstag.com/users/sign_out',
     apiUrl: 'https://api-observability-preprod.bsstag.com',
     baseDocUrl: 'https://preprod.bsstag.com',
     withCredentials: true
@@ -38,6 +42,7 @@ export default {
   production: {
     signInUrl:
       'https://api-observability.browserstack.com/api/v1/auth/start-sso',
+    signOutUrl: '',
     apiUrl: 'https://api-observability.browserstack.com',
     baseDocUrl: 'https://browserstack.com',
     withCredentials: true,
