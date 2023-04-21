13dc8ec0ed90d25c00106e1a30cc40b42e0d9577
f1uk3r <agarwal.umang4321@gmail.com>
2023-04-21 16:48:06 +0530

    slideover child stories removed

--- packages/bifrost/modules/SlideoverBody/index.jsx
+++ packages/bifrost/modules/SlideoverBody/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { twClassNames } from '@browserstack/utils';
 import PropTypes from 'prop-types';
 
-import './styles.scss';
-
 const SlideoverBody = ({ children, wrapperClassName }) => (
   <div
     className={twClassNames(`overflow-scroll py-4 flex-1`, wrapperClassName)}
