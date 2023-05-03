17a0cae690bf69668335ee122c485112d1b1f66f
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-05-03 17:44:06 +0530

    font size made 14 px for combobox and select menu

--- packages/bifrost/modules/ComboboxTrigger/index.jsx
+++ packages/bifrost/modules/ComboboxTrigger/index.jsx
@@ -95,7 +95,7 @@ const ComboboxTrigger = ({ onInputValueChange, placeholder, leadingIcon }) => {
           key={open || isLoading}
           placeholder={isLoading ? null : placeholder}
           className={twClassNames(
-            'cursor-pointer flex-1 focus:ring-0 focus-outline-0 focus-border-none bg-white border-0 flex-1 p-0 text-ellipsis pr-7',
+            'cursor-pointer flex-1 focus:ring-0 focus-outline-0 focus-border-none bg-white border-0 flex-1 p-0 text-ellipsis pr-7 text-sm leading-6',
             {
               'bg-base-50 cursor-not-allowed': disabled || isLoading,
               'pr-0': isTruncated
