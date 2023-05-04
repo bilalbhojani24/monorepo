2d9ca3d41ec4e44b770c919cd53e612751d9c10c
Siva K <sivakwalker3@gmail.com>
2023-05-04 10:42:15 +0530

    refactor: removed session test toggle in all other places

--- apps/o11y/src/features/TestDetails/containers/TestsLogsInfoTabs.jsx
+++ apps/o11y/src/features/TestDetails/containers/TestsLogsInfoTabs.jsx
@@ -3,7 +3,6 @@ import { useSelector } from 'react-redux';
 import { MdOutlineBugReport } from '@browserstack/bifrost';
 import { O11yButton, O11yTabs } from 'common/bifrostProxy';
 
-import SessionTestToggle from '../components/SessionTestToggle';
 import { useLogsContext } from '../contexts/LogsContext';
 import { useTestDetailsContentContext } from '../contexts/TestDetailsContext';
 import { getTestDetails } from '../slices/selectors';
@@ -49,19 +48,17 @@ const TestsLogsInfoTabs = () => {
           disableFullWidthBorder
           wrapperClassName="flex-1"
         />
-        <div className="flex items-center gap-3 pr-1">
-          {details.isValidVideo && <SessionTestToggle />}
-          <O11yButton
-            isIconOnlyButton
-            icon={<MdOutlineBugReport className="h-full w-full" />}
-            colors="white"
-            onClick={() => {
-              handleLogTDInteractionEvent({
-                interaction: 'report_bug_clicked'
-              });
-            }}
-          />
-        </div>
+        <O11yButton
+          isIconOnlyButton
+          icon={<MdOutlineBugReport className="h-full w-full" />}
+          colors="white"
+          onClick={() => {
+            handleLogTDInteractionEvent({
+              interaction: 'report_bug_clicked'
+            });
+          }}
+          wrapperClassName="pr-1"
+        />
       </div>
       <div className="flex-1">
         {activeTab.value === LOGS_INFO_TAB_KEYS.logs && (
