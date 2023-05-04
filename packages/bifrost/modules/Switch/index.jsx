e21e3f4c3a3980498f7260da5dac21367f40b113
shagunm12 <shagun@browserstack.com>
2023-05-04 22:03:13 +0530

    added disabled cursor

--- packages/bifrost/modules/Switch/index.jsx
+++ packages/bifrost/modules/Switch/index.jsx
@@ -35,6 +35,7 @@ const Switch = React.forwardRef(
                   'bg-base-200 relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-brand-500 focus:ring-offset-2',
                   headlessChecked ? 'bg-brand-600' : 'bg-base-200'
                 ],
+            'disabled:cursor-not-allowed',
             wrapperClassName
           )
         }
