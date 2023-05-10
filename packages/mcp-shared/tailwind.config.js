63d04f71a8e9d6663891537f0c6c93a5080e9414
Ashu <ashutosh.d@browserstack.com>
2023-05-10 14:33:28 +0530

    committing taildwing for eslint

--- packages/mcp-shared/tailwind.config.js
+++ packages/mcp-shared/tailwind.config.js
@@ -1,26 +1,10 @@
 const globalConfigs = require('@browserstack/tailwind-config');
 const tailwindSupportedScssNesting = require('@tailwindcss/nesting');
 
-const IS_DEV = process.argv.includes('IS_DEV');
-
-const mcsptTailwindConfig = {
+const mcpSharedTailwindConfig = {
   ...globalConfigs.globalTailwindConfig
 };
 
-mcsptTailwindConfig.content = [
-  'src/**/*.{js,jsx}',
-  '../../packages/bifrost/modules/**/*.{js,jsx}',
-  '../../packages/mcp-shared/features/**/*.{js,jsx}'
-];
+mcpSharedTailwindConfig.plugins.push(tailwindSupportedScssNesting);
 
-if (IS_DEV) {
-  mcsptTailwindConfig.safelist = [
-    {
-      pattern: /.*/
-    }
-  ];
-}
-
-mcsptTailwindConfig.plugins.push(tailwindSupportedScssNesting);
-
-module.exports = mcsptTailwindConfig;
+module.exports = mcpSharedTailwindConfig;
