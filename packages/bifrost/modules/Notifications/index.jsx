d1ab6229c4b9e021b38afd04a6cb63850b9cfa96
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-05-11 14:25:56 +0530

    notifications component added props body and footer node

--- packages/bifrost/modules/Notifications/index.jsx
+++ packages/bifrost/modules/Notifications/index.jsx
@@ -8,7 +8,11 @@ import { XMarkIcon } from '../Icon';
 const Notifications = forwardRef((props, ref) => {
   const {
     actionButtons,
+    body,
+    bodyClassName,
     description,
+    footer,
+    footerClassName,
     isCondensed,
     handleClose,
     headerIcon,
@@ -22,53 +26,77 @@ const Notifications = forwardRef((props, ref) => {
   }, [ref]);
 
   return (
-    <div className="pointer-events-auto flex w-full max-w-sm items-start rounded-lg bg-white p-4 shadow-lg ring-1 ring-black/5">
-      <div className="shrink-0 ">
-        {!isCondensed && headerIcon && headerIcon}
-      </div>
-      <div
-        className={twClassNames('w-0 flex-1 pt-0.5', {
-          'ml-3': !isCondensed
-        })}
-      >
-        <p className="text-base-900 line-clamp-2 break-words text-sm font-medium">
-          {title}
-        </p>
-
-        {!isCondensed && (
-          <p className="text-base-500 mt-1 break-keep text-sm">{description}</p>
+    <div className="pointer-events-auto max-w-sm rounded-lg bg-white shadow-lg ring-1 ring-black/5">
+      <div className="flex w-full items-start p-4 ">
+        {!isCondensed && headerIcon && (
+          <div className="mr-3 shrink-0">{headerIcon}</div>
         )}
 
-        {!isCondensed && actionButtons && (
-          <div className={twClassNames('mt-3 flex space-x-3')}>
+        <div className={twClassNames('w-0 flex-1 pt-0.5')}>
+          <p className="text-base-900 line-clamp-2 break-words text-sm font-medium">
+            {title}
+          </p>
+
+          {!isCondensed && description && (
+            <p className="text-base-500 mt-1 break-keep text-sm">
+              {description}
+            </p>
+          )}
+
+          {!isCondensed && actionButtons && (
+            <div className={twClassNames('mt-3 flex space-x-3')}>
+              {actionButtons?.(toastCtx?.toast || null)}
+            </div>
+          )}
+        </div>
+
+        {isCondensed && actionButtons && (
+          <div className="ml-3 flex shrink-0">
             {actionButtons?.(toastCtx?.toast || null)}
           </div>
         )}
+
+        <button
+          type="button"
+          className="text-base-400 hover:text-base-500 focus:ring-brand-500 ml-4 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-offset-2"
+          onClick={() => {
+            if (handleClose) handleClose(toastCtx?.toast || null);
+          }}
+        >
+          <span className="sr-only">Close</span>
+          <XMarkIcon className="h-5 w-5" aria-hidden="true" />
+        </button>
       </div>
 
-      {isCondensed && actionButtons && (
-        <div className="ml-3 flex shrink-0">
-          {actionButtons?.(toastCtx?.toast || null)}
+      {body && (
+        <div
+          className={twClassNames(
+            'p-4 ',
+            {
+              'rounded-md': !footer
+            },
+            bodyClassName
+          )}
+        >
+          {body}
         </div>
       )}
-
-      <button
-        type="button"
-        className="text-base-400 hover:text-base-500 focus:ring-brand-500 ml-4 rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-offset-2"
-        onClick={() => {
-          if (handleClose) handleClose(toastCtx?.toast || null);
-        }}
-      >
-        <span className="sr-only">Close</span>
-        <XMarkIcon className="h-5 w-5" aria-hidden="true" />
-      </button>
+      {footer && (
+        <div className={twClassNames('p-4 rounded-b-md', footerClassName)}>
+          {footer}
+        </div>
+      )}
     </div>
   );
 });
 
 Notifications.propTypes = {
   actionButtons: PropTypes.func,
+  body: PropTypes.node,
+  bodyClassName: PropTypes.string,
   description: PropTypes.string,
+  footer: PropTypes.node,
+  footerClassName: PropTypes.string,
   isCondensed: PropTypes.bool,
   handleClose: PropTypes.func,
   headerIcon: PropTypes.node,
@@ -77,12 +105,15 @@ Notifications.propTypes = {
 
 Notifications.defaultProps = {
   actionButtons: null,
-  description:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit oluptatum tenetur.',
+  body: null,
+  bodyClassName: '',
+  footer: null,
+  footerClassName: '',
+  description: '',
   isCondensed: false,
   handleClose: () => {},
   headerIcon: null,
-  title: 'Discussion moved'
+  title: ''
 };
 
 export default Notifications;
