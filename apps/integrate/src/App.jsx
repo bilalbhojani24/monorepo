e3a6146f2f71e0516ee2656e56fb2eb9dc96a5a2
rajeev-bstack <rajeev@browserstack.com>
2023-05-12 10:24:49 +0530

    INTG-103 | @rajeev-bstack | Remove commented code

--- apps/integrate/src/App.jsx
+++ apps/integrate/src/App.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import axios from 'axios';
 
 import env from './constants/envConstants';
 import { APP_ROUTES } from './constants/routesConstants';
 import useAuthRoutes from './hooks/useAuthRoutes';
 
-const initAPI = async () =>
-  // returns status code - 200 (uncomment and test)
-  axios.get('https://run.mocky.io/v3/ae5ce0d2-cecc-4580-8bdb-a91cd9d8db94');
+const initAPI = () => Promise.resolve('response');
 
-// returns status code - 401 (uncomment and test)
-// return axios.get(
-//   'https://run.mocky.io/v3/a1656866-98fe-49cd-9b97-1163c2866b48'
-// );
 const App = () => {
   const Routes = useAuthRoutes(
     APP_ROUTES,
