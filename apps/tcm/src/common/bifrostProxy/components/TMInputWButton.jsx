91c785d01ba1b242423443d95eda2f9333bbc9b9
ribinBS <ribin@browserstack.com>
2023-05-15 15:18:04 +0530

    inputw button fixes

--- apps/tcm/src/common/bifrostProxy/components/TMInputWButton.jsx
+++ apps/tcm/src/common/bifrostProxy/components/TMInputWButton.jsx
@@ -6,9 +6,15 @@ import { TMButton, TMInputField } from 'common/bifrostProxy';
 import PropTypes from 'prop-types';
 
 const TMInputWButton = forwardRef((props, ref) => (
-  <div className="relative z-0 flex w-full items-end justify-end">
+  <div className="relative z-0 flex w-full flex-wrap items-end justify-end">
+    {props?.label && (
+      <div className="text-base-700 mb-2 block w-full text-sm font-medium">
+        {props.label}
+      </div>
+    )}
     <TMInputField
       {...props}
+      label={null}
       ref={ref}
       wrapperClassName={twClassNames(
         'ribin flex-1 w-full [&_*]:rounded-none [&_*]:rounded-l-md',
@@ -31,12 +37,14 @@ const TMInputWButton = forwardRef((props, ref) => (
 
 TMInputWButton.propTypes = {
   wrapperClassName: PropTypes.string,
+  label: PropTypes.string,
   onButtonClick: PropTypes.func,
   buttonElement: PropTypes.node
 };
 
 TMInputWButton.defaultProps = {
   wrapperClassName: '',
+  label: '',
   onButtonClick: () => {},
   buttonElement: null
 };
