e0f3ba616015e8c5768390b1a4e40ff1a2dddd03
Abhishek Jain <ajj.2396@gmail.com>
2023-05-15 18:48:13 +0530

    getOnboardingEventsLogsData

--- apps/high-scale-testing/src/apis/constants/apiPaths.js
+++ apps/high-scale-testing/src/apis/constants/apiPaths.js
@@ -1,5 +1,11 @@
 const INIT_PATH = '/init';
 const ONBOARDING_DATA_PATH = '/onboarding';
+const ONBOARDING_EVENT_LOGS_DATA_PATH = `${ONBOARDING_DATA_PATH}/event-logs`;
 const SSO_PATH = '/auth/start-sso';
 
-export { INIT_PATH, ONBOARDING_DATA_PATH, SSO_PATH };
+export {
+  INIT_PATH,
+  ONBOARDING_DATA_PATH,
+  ONBOARDING_EVENT_LOGS_DATA_PATH,
+  SSO_PATH
+};
--- apps/high-scale-testing/src/apis/index.js
+++ apps/high-scale-testing/src/apis/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 
-import { ONBOARDING_DATA } from './constants/apiURLs';
+import {
+  ONBOARDING_DATA,
+  ONBOARDING_EVENT_LOGS_DATA
+} from './constants/apiURLs';
 
 const getOnboardingData = (userId) =>
   axios.get(ONBOARDING_DATA, {
@@ -9,4 +12,12 @@ const getOnboardingData = (userId) =>
     }
   });
 
-export { getOnboardingData };
+const getOnboardingEventsLogsData = (userId, onboardingType) =>
+  axios.get(ONBOARDING_EVENT_LOGS_DATA, {
+    params: {
+      userId,
+      onboardingType
+    }
+  });
+
+export { getOnboardingData, getOnboardingEventsLogsData };
