a1adf48b9667fc06cc0004d7f4c38f9c6c95c2b7
Abhishek Jain <ajj.2396@gmail.com>
2023-05-16 12:43:53 +0530

    Icon and CodeSnippet changes for EventLogs

--- apps/high-scale-testing/src/features/Onboarding/components/EventLogs.jsx
+++ apps/high-scale-testing/src/features/Onboarding/components/EventLogs.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   CodeSnippet,
   Hyperlink,
+  MdOutlineLocalCafe,
   MdOutlineOpenInNew,
   Modal,
   ProgressBar
@@ -13,7 +14,11 @@ const EventLogs = ({ eventLogsCode, currentStep, totalSteps }) => (
     <div className="mx-6 my-4">
       <p className="text-lg font-medium">Event Logs</p>
       <div className="border-base-300 mt-4 rounded-lg border">
-        <CodeSnippet code={eventLogsCode} maxHeight="260px" singleLine />
+        <CodeSnippet
+          code={eventLogsCode}
+          maxHeight="260px"
+          singleLine={false}
+        />
         <div className="border-base-300 border-y p-4">
           <ProgressBar
             currentStep="0"
@@ -31,7 +36,7 @@ const EventLogs = ({ eventLogsCode, currentStep, totalSteps }) => (
           />
         </div>
         <div className="text-base-600 flex gap-2 p-4 text-sm">
-          Icon{' '}
+          <MdOutlineLocalCafe className="text-2xl" />
           <p>
             Hang tight! We are completing the setup of your grid. It could take
             as long as 15-20m. You will also receive an email notification once
