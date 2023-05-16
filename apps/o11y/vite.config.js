4b61a04c439d269cec54d2474cfa4d55b9e08f99
Vishal Sanserwal <vishal.san@browserstack.com>
2023-05-16 17:41:12 +0530

    removed https from local dev

--- apps/o11y/vite.config.js
+++ apps/o11y/vite.config.js
@@ -1,4 +1,3 @@
-import basicSsl from '@vitejs/plugin-basic-ssl';
 import path from 'path';
 import { defineConfig } from 'vite';
 
@@ -24,10 +23,8 @@ export default defineConfig(({ command }) => {
   if (command !== 'build') {
     commonConfig.server = {
       port: 9000,
-      host: '127.0.0.1',
-      https: true
+      host: '127.0.0.1'
     };
-    commonConfig.plugins = [...productViteConfig.plugins, basicSsl()];
   }
   return commonConfig;
 });
