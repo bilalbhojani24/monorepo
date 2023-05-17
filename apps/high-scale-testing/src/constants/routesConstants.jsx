623f7a3cb84503d43a489c3334f9a50a8468b044
Abhishek Jain <ajj.2396@gmail.com>
2023-05-17 15:12:43 +0530

    Remove Counter and Dummy code

--- apps/high-scale-testing/src/constants/routesConstants.jsx
+++ apps/high-scale-testing/src/constants/routesConstants.jsx
@@ -1,35 +1,12 @@
 import React from 'react';
 
-import Counter from '../features/Counter';
 import Dashboard from '../features/GridConsole/components/Dashboard';
 import { Layout } from '../features/Layout';
 import { Onboarding } from '../features/Onboarding';
 
 import ROUTES from './routes';
 
-const Dummy = () => <h1>Hello world</h1>;
 export const APP_ROUTES = [
-  // Todo: Remove the Counter Router...
-  {
-    path: '/counter',
-    isProtected: true,
-    children: [
-      {
-        path: '/counter/new',
-        component: (
-          <>
-            <Counter />
-          </>
-        ),
-        isProtected: true
-      },
-      {
-        path: '/counter/dummy',
-        component: <Dummy />,
-        isProtected: true
-      }
-    ]
-  },
   {
     path: '/',
     isProtected: true,
