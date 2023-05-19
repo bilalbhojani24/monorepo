cfca340c52199f94504d1cc7d6a13edff54b9415
Siva K <sivakwalker3@gmail.com>
2023-05-19 10:48:46 +0530

    added export for each cta card shell components

--- packages/bifrost/index.js
+++ packages/bifrost/index.js
@@ -19,6 +19,10 @@ import ComboboxLabel from './modules/ComboboxLabel/index';
 import ComboboxOptionGroup from './modules/ComboboxOptionGroup/index';
 import ComboboxOptionItem from './modules/ComboboxOptionItem/index';
 import ComboboxTrigger from './modules/ComboboxTrigger/index';
+import CTACard from './modules/CTACard';
+import CTACardActions from './modules/CTACardActions';
+import CTACardContent from './modules/CTACardContent';
+import CTACardMedia from './modules/CTACardMedia';
 import DataVisualization from './modules/DataVisualization/index';
 import DateRangepicker from './modules/DateRangepicker';
 import DescriptionList from './modules/DescriptionList/index';
@@ -143,6 +147,10 @@ export {
   ComboboxOptionGroup,
   ComboboxOptionItem,
   ComboboxTrigger,
+  CTACard,
+  CTACardActions,
+  CTACardContent,
+  CTACardMedia,
   DataVisualization,
   DateRangepicker,
   DescriptionList,
