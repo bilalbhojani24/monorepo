aa4410e9f7a30eb89a71e9c0f1d1accf7c3b2931
samarthBrowserstack <samarth.s@browserstack.com>
2023-05-24 12:26:21 +0530

    Revert "Revert "Release version 8.0.0""
    This reverts commit d7f0e2b3a89e9e3be87520ac01daf36accaa624b.

--- packages/bifrost/modules/Resizable/Resizable.stories.jsx
+++ packages/bifrost/modules/Resizable/Resizable.stories.jsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
+// import { expect } from '@storybook/jest';
+// import { userEvent, within } from '@storybook/testing-library';
 import DocPageTemplate from '../../.storybook/DocPageTemplate';
+import { ControlledTree } from '../ListTree/ListTree.stories';
+import TruncateText from '../TruncateText';
 
 import Resizable from './index';
 
@@ -73,11 +77,118 @@ const defaultConfig = {
   },
   controls: {}
 };
+
+const listTreeDemoDataSet = [
+  {
+    name: 'file A',
+    contents: [
+      {
+        name: (
+          <TruncateText wrapperClassName="line-clamp-1">
+            file A-1 Really long file name case. Lorem Ipsum is simply dummy
+            text of the printing and typesetting industry. Lorem Ipsum has been
+            the industrys standard dummy text ever since the 1500s, when an
+            unknown printer took a galley of type and scrambled it to make a
+            type specimen book. It has survived not only five centuries, but
+            also the leap into electronic typesetting, remaining essentially
+            unchanged.
+          </TruncateText>
+        ),
+        contents: null
+      },
+      {
+        name: 'file A-2',
+        contents: [
+          {
+            name: 'file A-2-a',
+            contents: null
+          }
+        ]
+      }
+    ]
+  },
+  {
+    name: 'file 2',
+    contents: [
+      {
+        name: 'file 2a',
+        contents: null
+      },
+      {
+        name: 'file 2b',
+        contents: [
+          {
+            name: 'file 2b1',
+            contents: null
+          }
+        ]
+      }
+    ]
+  }
+];
+
+const ResizeableTreeExample = () => {
+  const resizeRef = useRef(null);
+  const onResizeStart = () => {
+    resizeRef.current.style.opacity = 1;
+  };
+  const onResizeStop = () => {
+    resizeRef.current.removeAttribute('style');
+  };
+  return (
+    <Resizable
+      resizeHandles={['e']}
+      handleSize={[6, 1]}
+      width={300}
+      minConstraints={[300]}
+      maxConstraints={[900]}
+      onResizeStart={onResizeStart}
+      onResizeStop={onResizeStop}
+      handle={(__resizeHandleAxis, ref) => (
+        <span
+          className="group absolute right-0 top-0 h-full translate-x-1.5 px-1 hover:cursor-col-resize"
+          ref={ref}
+        >
+          <span
+            className="bg-brand-600 block h-full w-0.5 opacity-0 transition-opacity duration-500 group-hover:opacity-100"
+            ref={resizeRef}
+          />
+        </span>
+      )}
+      className="border-base-300 relative h-full border border-solid p-4"
+    >
+      <ControlledTree data={listTreeDemoDataSet} indent={1} />
+    </Resizable>
+  );
+};
+
 const Template = (args) => <Resizable {...args} />;
+
+const ResizeableTreeTemplate = () => (
+  <ResizeableTreeExample data={listTreeDemoDataSet} />
+);
+
 const Primary = Template.bind({});
+const ResizeableTree = ResizeableTreeTemplate.bind({});
+// ResizeableTree.play = async ({ canvasElement }) => {
+//   const canvas = within(canvasElement);
+//   let treeItems = canvas.queryAllByRole('treeitem');
+//   await expect(treeItems.length).toBe(7);
+//   await userEvent.hover(canvas.queryAllByLabelText('menu-button')[0]);
+//   await userEvent.click(canvas.queryAllByLabelText('menu-button')[0]);
+//   const buttons = canvas.queryAllByRole('button');
+//   await userEvent.click(buttons[1]);
+//   await userEvent.click(buttons[9]);
+//   await userEvent.click(buttons[11]);
+//   treeItems = canvas.queryAllByRole('treeitem');
+//   await expect(treeItems.length).toBe(10);
+//   const tooltips = canvas.queryAllByRole('tooltip');
+//   await userEvent.hover(tooltips[3]);
+// };
+
 Primary.parameters = {
   controls: {}
 };
 
 export default defaultConfig;
-export { Primary };
+export { Primary, ResizeableTree };
--- packages/bifrost/modules/Steps/index.jsx
+++ packages/bifrost/modules/Steps/index.jsx
@@ -422,7 +422,8 @@ const Steps = ({ label, onClick, steps, format, wrapperClassName }) => {
             format === STEPS_FORMAT[2],
           'py-12 px-4 sm:px-6 lg:px-8': format === STEPS_FORMAT[4]
         },
-        wrapperClassName
+        wrapperClassName,
+        'z-10'
       )}
     >
       <nav
