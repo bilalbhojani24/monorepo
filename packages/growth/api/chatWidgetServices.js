d20d877ef4425a9703c51c1224cc1a7cb0047220
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-05-25 18:52:27 +0530

    Endpoint URL for chatwidget service for prod and staging

--- packages/growth/api/chatWidgetServices.js
+++ packages/growth/api/chatWidgetServices.js
@@ -2,9 +2,8 @@ import axiosInstance from './axiosInstance';
 
 const chatWidgetService = {
   getChatWidgetInitialisation: async () => {
-    const response = await axiosInstance.get(
-      'https://k8s-devos.bsstag.com/user/fresh-chat-config'
-    );
+    const response = await axiosInstance.get('user/fresh-chat-config');
+
     return response.data;
   }
 };
