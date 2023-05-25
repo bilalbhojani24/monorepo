90930b4c498f3f7200144bef42063a78b5553c8a
ritikaGupta4 <ritika.g@browserstack.com>
2023-05-25 16:48:55 +0530

    baseUrl

--- apps/high-scale-testing/src/api/utils/getBaseURL.js
+++ apps/high-scale-testing/src/api/utils/getBaseURL.js
@@ -1,7 +1,3 @@
-// const getBaseURL = (version = 'v1') => `/api/${version}`;
-
-const getBaseURL = (version = 'v1') =>
-  `https://localhost:8082/high-scale-testing/${version}`;
-// `http://localhost:5000/api/${version}`;
+const getBaseURL = (version = 'v1') => `/api/${version}`;
 
 export { getBaseURL };
