3a5289327be76d2e9a44bd1727272c5ccd694275
samarthBrowserstack <samarth.s@browserstack.com>
2023-05-29 04:18:00 +0530

    Add onlyIconOrText story in breadcrumbs

--- packages/bifrost/modules/Breadcrumb/const/breadcrumbConstants.jsx
+++ packages/bifrost/modules/Breadcrumb/const/breadcrumbConstants.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+
 import { HomeIcon } from '../../Icon';
 
 export const BREADCRUMB_SIZE = {
   fullWidth: 'full-width',
   contained: 'contained',
-  default: 'default',
+  default: 'default'
 };
 
 export const breadcrumbData = [
@@ -12,18 +13,36 @@ export const breadcrumbData = [
     name: 'Home',
     url: '/',
     current: false,
-    icon: <HomeIcon className="h-5 w-5 flex-shrink-0 mr-2" aria-hidden="true" />,
+    icon: <HomeIcon className="mr-2 h-5 w-5 shrink-0" aria-hidden="true" />
   },
   {
     name: 'Project Nero',
     url: '/',
     current: false,
-    icon: <HomeIcon className="h-5 w-5 flex-shrink-0 mr-2" aria-hidden="true" />,
+    icon: <HomeIcon className="mr-2 h-5 w-5 shrink-0" aria-hidden="true" />
   },
   {
     name: 'Current Page',
     url: '/',
     current: true,
-    icon: <HomeIcon className="h-5 w-5 flex-shrink-0 mr-2" aria-hidden="true" />,
+    icon: <HomeIcon className="mr-2 h-5 w-5 shrink-0" aria-hidden="true" />
+  }
+];
+
+export const onlyIconOrTextBreadcrumbData = [
+  {
+    url: '/',
+    current: false,
+    icon: <HomeIcon className="ml-2 h-5 w-5 shrink-0" aria-hidden="true" />
   },
+  {
+    name: 'Project Nero',
+    url: '/',
+    current: false
+  },
+  {
+    name: 'Current Page',
+    url: '/',
+    current: true
+  }
 ];
