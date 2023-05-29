517aaca7f9f19b5e90fe5bd3e420c99925ff5773
rajeev-bstack <rajeev@browserstack.com>
2023-05-29 11:08:02 +0530

    INTG-196 | @rajeev-bstack | Update select config options styling

--- apps/integrate/src/features/Overview/components/SelectConfigurations.jsx
+++ apps/integrate/src/features/Overview/components/SelectConfigurations.jsx
@@ -1,31 +1,80 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   Dropdown,
   DropdownOptionGroup,
   DropdownOptionItem,
-  DropdownTrigger
+  DropdownTrigger,
+  MdCheck
 } from '@browserstack/bifrost';
+import { twClassNames } from '@browserstack/utils';
 
 import {
+  activeConfigurationsSelector,
   configurationsSelector,
   setActiveConfigurations
 } from '../../../globalSlice';
 
 const SelectConfigurations = () => {
   const dispatch = useDispatch();
+
+  const activeConfigurations = useSelector(activeConfigurationsSelector);
+  const getIsConfigurationSelected = useCallback(
+    (configuration) => {
+      if (activeConfigurations?.length > 1 && configuration.value === 'all') {
+        return true;
+      }
+      return !!(
+        activeConfigurations?.length === 1 &&
+        configuration.value === activeConfigurations[0].value
+      );
+    },
+    [activeConfigurations]
+  );
+
   const AllConfigurationsOption = {
     id: 'all',
-    body: <p>All</p>
+    body: (
+      <p className="flex">
+        <p className="mr-2 w-4">
+          {getIsConfigurationSelected({ value: 'all' }) && (
+            <MdCheck className="text-brand-500 mr-1 text-xl" />
+          )}
+        </p>
+        <span
+          className={twClassNames({
+            'font-bold': getIsConfigurationSelected({ value: 'all' })
+          })}
+        >
+          All
+        </span>
+      </p>
+    )
   };
   const configurations = useSelector(configurationsSelector);
   const options = useMemo(
     () =>
       configurations?.map((configuration) => ({
         id: configuration.value,
-        body: <p>{configuration.label}</p>
+        body: (
+          <p className="flex">
+            <p className="mr-2 w-4">
+              {getIsConfigurationSelected(configuration) && (
+                <MdCheck className="text-brand-500 text-xl" />
+              )}
+            </p>
+
+            <span
+              className={twClassNames('overflow-hidden truncate', {
+                'font-bold': getIsConfigurationSelected(configuration)
+              })}
+            >
+              {configuration.label}
+            </span>
+          </p>
+        )
       })) ?? [],
-    [configurations]
+    [configurations, getIsConfigurationSelected]
   );
 
   const selectConfiguration = (val) => {
