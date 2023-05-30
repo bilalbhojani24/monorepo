ae14f68de24ef78d1e1673714b3345429e839533
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-05-30 18:29:22 +0530

    vite config changed

--- apps/growth-demo-app/vite.config.js
+++ apps/growth-demo-app/vite.config.js
@@ -1,22 +1,6 @@
-import fs from 'fs';
 import { defineConfig } from 'vite';
 
 const { productViteConfig } = require('@browserstack/vite-config');
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const commonConfig = {
-    ...productViteConfig
-  };
-  if (command !== 'build') {
-    commonConfig.server = {
-      https: {
-        key: fs.readFileSync('./certs/bsstag.com.key'),
-        cert: fs.readFileSync('./certs/bsstag.com.crt')
-      },
-      port: 9001,
-      host: '127.0.0.1'
-    };
-  }
-  return commonConfig;
-});
+export default defineConfig(() => ({ ...productViteConfig }));
