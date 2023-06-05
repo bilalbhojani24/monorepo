81707534521194240a0bae876e151a55de14e01a
Abhishek Jain <ajj.2396@gmail.com>
2023-06-05 14:55:31 +0530

    Setup Sentry for HST

--- apps/high-scale-testing/src/hooks/useApp.js
+++ apps/high-scale-testing/src/hooks/useApp.js
@@ -1,13 +1,16 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { initErrorLogger } from '@browserstack/utils';
 import { INIT_URL } from 'api/constants/apiURLs';
 import axios from 'axios';
+import { SENTRY_DSN } from 'constants/keys';
 import { initialiseApplication } from 'globalSlice';
 import { getUserDetails } from 'globalSlice/selector';
-import { getEnvConfig } from 'utils/common';
+import { getEnv, getEnvConfig } from 'utils/common';
 
 const useApp = () => {
   const dispatch = useDispatch();
+  const env = getEnv();
   const envConfig = getEnvConfig();
 
   const { enableSentry } = envConfig;
@@ -22,7 +25,28 @@ const useApp = () => {
     return response;
   };
 
-  useEffect(() => {}, [userDetails]);
+  useEffect(() => {
+    if (enableSentry) {
+      initErrorLogger({
+        dsn: SENTRY_DSN,
+        debug: false,
+        release: 'v1.0.0-hst',
+        environments: env,
+        tracesSampleRate: 1.0,
+        denyUrls: [
+          // Ignoring errors getting generated from Chrome extensions as these are not to be logged under our sentry env.
+          /extensions\//i,
+          /^chrome:\/\//i,
+          /extension:\//i,
+          // Ignoring VWO related errors as there is no specific library upgrade which can resolve the errors.
+          // Also the errors we are getting are more or less specfic to some of the users.
+          /https:\/\/dev.visualwebsiteoptimizer.com\/.*/gi,
+          // Ignore errors getting raised from freshchat widget related code.
+          /https:\/\/wchat.freshchat.com\/.*/gi
+        ]
+      });
+    }
+  }, [enableSentry, env, userDetails]);
   return {
     initAPI
   };
--- apps/high-scale-testing/src/utils/common.js
+++ apps/high-scale-testing/src/utils/common.js
@@ -1,5 +1,26 @@
-const getEnvConfig = (config = import.meta.env.BSTACK_STAGE) => {
-  console.log('Log: config:', config);
+import configMappings from 'constants/configMappings';
+
+const getEnv = (stage = import.meta.env.BSTACK_STAGE) => {
+  let guessedStage = 'local';
+  if (!stage) {
+    const { hostname } = window.location;
+
+    if (hostname.endsWith('browserstack.com')) {
+      guessedStage = 'production';
+    } else if (hostname.includes('local')) {
+      guessedStage = 'local';
+    } else {
+      guessedStage = 'staging';
+    }
+  }
+
+  return guessedStage;
 };
 
-export { getEnvConfig };
+const getEnvConfig = (stage = import.meta.env.BSTACK_STAGE) => {
+  const guessedStage = stage || getEnv();
+
+  return configMappings[guessedStage];
+};
+
+export { getEnv, getEnvConfig };
