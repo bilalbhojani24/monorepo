05c74f6a8cf1e56b61375fff51dcffe2ec7ef2c3
Ashish <ashish030517@gmail.com>
2023-06-05 09:52:36 +0530

    using withCredential for all env

--- packages/growth/api/axiosInstance.js
+++ packages/growth/api/axiosInstance.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
 const environment = window.location.host;
-const isProd = environment.includes('.browserstack.com');
-const baseURL = isProd
+const baseURL = environment.includes('.browserstack.com')
   ? 'https://browserstack.com/'
   : 'https://k8s-devos.bsstag.com/';
 
 const axiosInstance = axios.create({
   baseURL,
-  withCredentials: !isProd
+  withCredentials: true
 });
 
 export default axiosInstance;
