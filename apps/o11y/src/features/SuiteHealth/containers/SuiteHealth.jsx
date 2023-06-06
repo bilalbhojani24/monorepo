04014bc74c4855b9c20552e9d4e2988f7409195d
Vishal Sanserwal <vishal.san@browserstack.com>
2023-06-06 18:23:46 +0530

    tab name updated

--- apps/o11y/src/features/SuiteHealth/containers/SuiteHealth.jsx
+++ apps/o11y/src/features/SuiteHealth/containers/SuiteHealth.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
+import { SNP_PARAMS_MAPPING } from 'constants/common';
 
 import { TABS_KEY_MAPPING } from '../constants';
 
 function SuiteHealth() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  if (searchParams.get('tab') === TABS_KEY_MAPPING.unique_errors) {
+  if (
+    searchParams.get(SNP_PARAMS_MAPPING.snpTab) ===
+    TABS_KEY_MAPPING.unique_errors
+  ) {
     return (
       <Navigate
         to={`${location.pathname}/unique_errors/?${searchParams.toString()}`}
