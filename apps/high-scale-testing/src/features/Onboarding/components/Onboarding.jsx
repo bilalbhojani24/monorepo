819a87fdb0ee296379ade9f0754698c927b71f9c
Abhishek Jain <ajj.2396@gmail.com>
2023-06-06 13:14:17 +0530

    Desifn Visual Review

--- apps/high-scale-testing/src/features/Onboarding/components/Onboarding.jsx
+++ apps/high-scale-testing/src/features/Onboarding/components/Onboarding.jsx
@@ -6,7 +6,6 @@ import {
   CodeSnippet,
   Hyperlink,
   ListFeedsNode,
-  MdCached,
   MdOutlineOpenInNew,
   PageHeadings,
   RadioGroup,
@@ -18,7 +17,8 @@ import {
   Tabs
 } from '@browserstack/bifrost';
 import { twClassNames } from '@browserstack/utils';
-import HourglassBottomOutlinedIcon from '@mui/icons-material/HourglassBottomOutlined';
+import { OpenInNew } from '@mui/icons-material';
+import LoaderGif from 'assets/icons/loader.gif';
 import { EVENT_LOGS_STATUS } from 'constants/onboarding';
 
 import EventLogs from './EventLogs';
@@ -115,7 +115,7 @@ const Onboarding = () => {
   );
 
   const DescriptionNodeStep1 = (
-    <div className="my-4">
+    <div className="mb-4 mt-2">
       <RadioGroup
         onChange={(e, option) => {
           const newOption = SCRATCH_RADIO_GROUP_OPTIONS.find(
@@ -131,7 +131,7 @@ const Onboarding = () => {
   );
 
   const DescriptionNodeStep2 = (
-    <div className="my-4 w-2/5">
+    <div className="mb-4 mt-2 w-2/5">
       <SelectMenu
         disabled={eventLogsCode && eventLogsCode.length > 0}
         onChange={(e) => {
@@ -150,22 +150,36 @@ const Onboarding = () => {
   );
 
   const DescriptionNodeStep3 = (
-    <p className="text-base-700 mb-4 mt-1 text-sm">
-      Set up a new IAM role via the CloudFormation link and generate the AWS
-      access key and secret to create and manage the Automation Grid. Read more
-      about this{' '}
-      <Hyperlink target="_blank" href="/" className="inline">
-        here
-      </Hyperlink>
-      .
-    </p>
+    <div className="mb-4">
+      <p className="text-base-700 mb-2 mt-1 text-sm">
+        Set up a new IAM role via the CloudFormation link and generate the AWS
+        access key and secret to create and manage the Automation Grid. Read
+        more about this{' '}
+        <Hyperlink target="_blank" href="/" className="inline">
+          here
+        </Hyperlink>
+        .
+      </p>
+      <Button
+        colors="white"
+        icon={<OpenInNew />}
+        onClick={() => {
+          window.location.href = 'www.browserstack.com';
+          return null;
+        }}
+        modifier="primary"
+        variant="rounded"
+      >
+        Cloud Formation Link
+      </Button>
+    </div>
   );
 
   const DescriptionNodeStep4 = (
     <div className="m-4">
       {/* eslint-disable-next-line tailwindcss/no-arbitrary-value */}
       <ol className="text-base-500 list-[lower-alpha] text-sm">
-        <li className="text-base-900 py-2">
+        <li className="text-base-900 pb-2">
           <div>
             <p className="text-base-900 mb-2">
               {CODE_SNIPPETS_SCRATCH['create-grid'].a.text}
@@ -205,7 +219,13 @@ const Onboarding = () => {
             handleLinkClick={() => {}}
             linkText=""
             modifier="primary"
-            description="This command will create an EKS cluster ‘high-scale-grid-cluster’ with a grid named ‘high-scale-grid’ that supports a concurrency of ‘50’ browsers sessions. The instance type for worker nodes will be ‘m7g.medium (vCPU-1, memory-8GB)’."
+            title="Grid Details"
+            description={[
+              'Grid name: ‘high-scale-grid’',
+              'Concurrent browser sessions: ‘50’',
+              'Worker nodes instance type: ‘m7g.medium (vCPU-1, memory-8GB)’',
+              'Cluster name: ‘high-scale-grid-cluster’'
+            ]}
           />
         </div>
         <CodeSnippet
@@ -219,7 +239,7 @@ const Onboarding = () => {
 
   const HeaderNodeStep1 = (
     <div className="flex">
-      <p className="text-base-900 text-base font-semibold">
+      <p className="text-base-900 text-sm font-semibold">
         Choose Cloud Provider
       </p>
     </div>
@@ -228,7 +248,7 @@ const Onboarding = () => {
   const HeaderNodeStep2 = (
     <>
       <div className="flex gap-2">
-        <p className="text-base-900 text-base font-semibold">
+        <p className="text-base-900 text-sm font-semibold">
           Grid Profile Details
         </p>
         <Badge hasRemoveButton={false} modifier="warn" text="Default" />
@@ -242,14 +262,14 @@ const Onboarding = () => {
 
   const HeaderNodeStep3 = (
     <div className="flex">
-      <p className="text-base-900 text-base font-semibold">Setup IAM Role</p>
+      <p className="text-base-900 text-sm font-semibold">Setup IAM Role</p>
     </div>
   );
 
   const HeaderNodeStep4 = (
     <>
       <div className="flex">
-        <p className="text-base-900 text-base font-semibold">Create Grid</p>
+        <p className="text-base-900 text-sm font-semibold">Create Grid</p>
       </div>
       <p className="text-base-700 mt-1 text-sm">
         Execute the below commands to setup the BrowserStack CLI and create an
@@ -309,7 +329,7 @@ const Onboarding = () => {
   );
 
   return (
-    <div className="border-base-300 m-auto my-10 w-4/6 max-w-5xl rounded-lg border">
+    <div className="border-base-300 m-auto my-10 w-4/6 max-w-4xl rounded-lg border">
       <PageHeadings
         actions={
           <>
@@ -338,19 +358,16 @@ const Onboarding = () => {
           'overflow-auto bg-white border-base-300 px-7 ',
           {
             'h-[calc(100vh-112px-140px-48px-40px)]': onboardingStep > 0,
-            'py-6':
+            'pb-6':
               onboardingStep === 0 ||
               (onboardingStep === 1 &&
-                onboardingType !== ONBOARDING_TYPES.scratch),
-            'pt-6':
-              onboardingStep === 1 &&
-              onboardingType === ONBOARDING_TYPES.scratch
+                onboardingType !== ONBOARDING_TYPES.scratch)
           }
         )}
       >
         {onboardingStep === 0 && (
           <>
-            <h3 className="text-base-900 mb-4 flex gap-x-2 text-base font-medium leading-6">
+            <h3 className="text-base-900 mb-2 flex gap-x-2 text-base font-medium leading-6">
               Do you have an existing Kubernetes setup?
             </h3>
             <RadioStackedCard
@@ -372,7 +389,7 @@ const Onboarding = () => {
         {onboardingStep === 1 &&
           onboardingType === ONBOARDING_TYPES.existing && (
             <>
-              <p className="text-base-900 font-semibold">Grid Setup</p>
+              <p className="text-base-900 text-sm font-semibold">Grid Setup</p>
               <p className="text-base-900 mt-1 text-sm">
                 Execute the below commands to initialise grid creation.
               </p>
@@ -414,15 +431,19 @@ const Onboarding = () => {
         (onboardingType === ONBOARDING_TYPES.scratch ||
           onboardingType === ONBOARDING_TYPES.existing) &&
         (((!eventLogsCode || eventLogsCode?.length === 0) && (
-          <div className="bg-base-50 text-base-700 flex gap-2 px-6 py-3">
-            <HourglassBottomOutlinedIcon /> Waiting for you to complete the
-            above steps to connect the grid.
+          <div className="bg-base-50 text-base-900 flex gap-2 px-6 py-4 text-sm">
+            <div>
+              <img src={LoaderGif} alt="" width={20} height={20} />
+            </div>{' '}
+            Waiting for you to complete the above steps to connect the grid.
           </div>
         )) ||
           (eventLogsCode && eventLogsCode.length > 0 && showGridHeartBeats && (
             <div className="text-base-700 flex gap-2 px-6 py-3">
-              <HourglassBottomOutlinedIcon /> Grid heartbeats detected.
-              Initialising events log...
+              <div>
+                <img src={LoaderGif} alt="" width={20} height={20} />
+              </div>{' '}
+              Grid heartbeats detected. Initialising events log...
             </div>
           )))}
 
@@ -434,7 +455,9 @@ const Onboarding = () => {
         !showGridHeartBeats && (
           <div className="flex justify-between px-6 py-3">
             <div className="text-base-700 flex gap-2">
-              <MdCached />
+              <div>
+                <img src={LoaderGif} alt="" width={20} height={20} />
+              </div>
               ‘high-scale-grid’ grid creation is in progress...
             </div>
             <Button colors="white" onClick={viewEventLogsClickHandler}>
