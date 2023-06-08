fdcf1391b92a485430ef46cf8ecf79e3b8675faf
Abhishek Jain <ajj.2396@gmail.com>
2023-06-08 16:15:12 +0530

    Fix the sign out URL

--- apps/high-scale-testing/src/constants/configMappings.js
+++ apps/high-scale-testing/src/constants/configMappings.js
@@ -1,13 +1,17 @@
 const configMappings = {
   local: {
+    baseUrl: '',
     enableAnalytics: false,
     enableSentry: false
   },
   production: {
+    baseUrl: 'https://www.browserstack.com',
+    docHomeURL: 'https://browserstack.com/docs',
     enableAnalytics: true,
     enableSentry: true
   },
   staging: {
+    baseUrl: '',
     enableAnalytics: false,
     enableSentry: true
   }
