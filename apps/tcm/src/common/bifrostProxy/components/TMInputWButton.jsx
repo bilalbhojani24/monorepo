c2925b34ffcbe85c505983efcdb01a37d1bd7c48
arsalanPro <arsalan.k@browserstack.com>
2023-06-13 20:09:52 +0530

    input w button fix

--- apps/tcm/src/common/bifrostProxy/components/TMInputWButton.jsx
+++ apps/tcm/src/common/bifrostProxy/components/TMInputWButton.jsx
@@ -6,36 +6,36 @@ import { TMButton, TMInputField } from 'common/bifrostProxy';
 import PropTypes from 'prop-types';
 
 const TMInputWButton = forwardRef((props, ref) => (
-  <div className="relative z-0 flex basis-full items-end justify-end">
+  <div className="relative z-0 flex w-full flex-col">
     {props?.label && (
-      <div className="text-base-700 mb-2 block w-full text-sm font-medium">
+      <div className="text-base-700 mb-2 flex text-sm font-medium">
         {props.label}
       </div>
     )}
-    <div className="basis-11/12">
-      <TMInputField
-        {...props}
-        label={null}
-        ref={ref}
+    <div className="flex w-full">
+      <div className="flex-1">
+        <TMInputField
+          {...props}
+          label={null}
+          ref={ref}
+          wrapperClassName={twClassNames(
+            '[&_*]:rounded-none [&_*]:rounded-l-md',
+            props.wrapperClassName
+          )}
+        />
+      </div>
+      <TMButton
+        onClick={props?.onButtonClick}
+        colors="white"
         wrapperClassName={twClassNames(
-          'flex-1 [&_*]:rounded-none [&_*]:rounded-l-md',
-          props.wrapperClassName
+          'whitespace-nowrap inline-flex bg-base-50 text-base-700 rounded-none rounded-r-md border border-l-0 focus:ring-offset-0 focus:outline-none focus:z-10'
         )}
-      />
+      >
+        {props.buttonElement}
+      </TMButton>
     </div>
-    <TMButton
-      onClick={props?.onButtonClick}
-      colors="white"
-      wrapperClassName={twClassNames(
-        'basis-1/12 whitespace-nowrap inline-flex bg-base-50 text-base-700 rounded-none rounded-r-md border border-l-0 focus:ring-offset-0 focus:outline-none focus:z-10'
-      )}
-    >
-      {props.buttonElement}
-    </TMButton>
   </div>
 ));
-
-// space-x-2 rounded-r-md border px-4 py-2 text-sm font-medium focus:outline-none focus:ring-1
 
 TMInputWButton.propTypes = {
   wrapperClassName: PropTypes.string,
