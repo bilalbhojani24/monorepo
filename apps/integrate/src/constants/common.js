79b326e78edb30d10ac6688bd0fd2ba223cffd28
rajeev-bstack <rajeev@browserstack.com>
2023-06-14 12:24:32 +0530

    INTG-228 | @rajeev-bstack | Revert env url

--- apps/integrate/src/constants/common.js
+++ apps/integrate/src/constants/common.js
@@ -1,7 +1,7 @@
 export const BSTACK_TOPNAV_ELEMENT_ID = 'integrate-header-id';
 export const GETTING_STARTED_DOC_LINK = '/docs/onboarding/integrations';
 export const BSTACK_PROD = 'https://www.browserstack.com';
-export const BSTACK_DEVINT = 'https://k8s.bsstag.com';
+export const BSTACK_DEVINT = 'https://devint.bsstag.com';
 
 export const URL_REGEX =
   /* eslint-disable-next-line */
