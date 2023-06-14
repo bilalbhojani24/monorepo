8ca21975840d88186c3aa2f2ff0829bea7261738
Sarosh Malek <sarosh@browserstack.com>
2023-06-14 15:31:11 +0530

    remove screen reader mock devices

--- apps/accessibility/src/features/ScreenReader/useScreenReader.js
+++ apps/accessibility/src/features/ScreenReader/useScreenReader.js
@@ -1,8 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import fetchScreenReaderDevices, {
-  fetchMockData
-} from 'api/fetchScreenReaderDevices';
+import fetchScreenReaderDevices from 'api/fetchScreenReaderDevices';
 import {
   TRIAL_EXPIRED,
   TRIAL_FAILED,
@@ -60,8 +58,7 @@ export default function useScreenReader(noOfDevices) {
   };
 
   useEffect(() => {
-    fetchMockData().then(setDeviceCombinations);
-    // fetchScreenReaderDevices().then(setDeviceCombinations);
+    fetchScreenReaderDevices().then(setDeviceCombinations);
   }, []);
 
   useEffect(() => {
