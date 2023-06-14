cca525fa986f874e2292859e6ad627701ae5f2bc
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-06-14 15:32:00 +0530

    initial focus prop added and default to first focusable element

--- packages/bifrost/modules/Slideover/index.jsx
+++ packages/bifrost/modules/Slideover/index.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useCallback, useEffect } from 'react';
+import React, { Fragment, useCallback, useEffect, useRef } from 'react';
 import { twClassNames } from '@browserstack/utils';
 import { Transition } from '@headlessui/react';
 import PropTypes from 'prop-types';
@@ -9,7 +9,9 @@ import { XMarkIcon } from '../Icon';
 import { MODAL_SIZE } from './const/modalConstants';
 import useSlideover from './useSlideover';
 
-const Slideover = (props) => {
+const Slideover = React.forwardRef((props) => {
+  const containerRef = useRef();
+
   const {
     children,
     onCloseWithOutsideButton,
@@ -19,7 +21,8 @@ const Slideover = (props) => {
     backgroundOverlay,
     size,
     closeButtonOutside,
-    topMarginElementId
+    topMarginElementId,
+    initialFocusRef
   } = props;
 
   const { marginTopAdjustment } = useSlideover(topMarginElementId);
@@ -40,94 +43,119 @@ const Slideover = (props) => {
     };
   }, [handleIfEscapeClicked]);
 
+  useEffect(() => {
+    if (show) {
+      if (initialFocusRef && initialFocusRef.current) {
+        setTimeout(() => {
+          initialFocusRef.current.focus();
+        }, 0);
+      } else {
+        const containerNode = containerRef.current;
+        const focusableEls = containerNode.querySelectorAll(
+          'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select'
+        );
+        const firstFocusableEl = focusableEls[0];
+        setTimeout(() => {
+          firstFocusableEl?.focus();
+        }, 0);
+      }
+    }
+  }, [initialFocusRef, show]);
+
   return (
-    <Transition show={show} unmount={false}>
-      {backgroundOverlay ? (
+    <div ref={containerRef}>
+      <Transition show={show} unmount={false}>
+        {backgroundOverlay ? (
+          <Transition.Child
+            appear="true"
+            unmount={false}
+            enter="ease-in duration-500"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-500"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div
+              role="presentation"
+              style={{ marginTop: marginTopAdjustment }}
+              className={twClassNames(`bg-base-500 fixed inset-0 z-10`, {
+                'opacity-75': backgroundOverlay,
+                'opacity-0': !backgroundOverlay
+              })}
+              onClick={() => {
+                onOverlayClick?.();
+              }}
+            />
+          </Transition.Child>
+        ) : null}
+
         <Transition.Child
+          as={Fragment}
           appear="true"
           unmount={false}
-          enter="ease-in duration-500"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="ease-in duration-500"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
+          enter="transform transition ease-out duration-500"
+          enterFrom="translate-x-full"
+          enterTo="translate-x-0"
+          leave="transform transition ease-out duration-500"
+          leaveFrom="translate-x-0"
+          leaveTo="translate-x-full"
         >
           <div
-            role="presentation"
+            className="fixed inset-y-0 right-0 z-10 flex items-start"
             style={{ marginTop: marginTopAdjustment }}
-            className={twClassNames(`bg-base-500 fixed inset-0 z-10`, {
-              'opacity-75': backgroundOverlay,
-              'opacity-0': !backgroundOverlay
-            })}
-            onClick={() => {
-              onOverlayClick?.();
-            }}
-          />
-        </Transition.Child>
-      ) : null}
-
-      <Transition.Child
-        as={Fragment}
-        appear="true"
-        unmount={false}
-        enter="transform transition ease-out duration-500"
-        enterFrom="translate-x-full"
-        enterTo="translate-x-0"
-        leave="transform transition ease-out duration-500"
-        leaveFrom="translate-x-0"
-        leaveTo="translate-x-full"
-      >
-        <div
-          className="fixed inset-y-0 right-0 z-10 flex items-start"
-          style={{ marginTop: marginTopAdjustment }}
-        >
-          {closeButtonOutside && (
-            <div className="flex pt-4 pr-1">
-              <Button
-                colors="white"
-                size="large"
-                variant="minimal"
-                onClick={() => {
-                  onCloseWithOutsideButton?.();
-                }}
-                aria-label="close icon"
-              >
-                <XMarkIcon
-                  className="text-base-300 h-6 w-6"
-                  aria-hidden="true"
-                />
-              </Button>
-            </div>
-          )}
-
-          <div
-            className={twClassNames(
-              `relative flex h-full flex-col overflow-auto bg-white shadow-xl w-screen inset-0`,
-              {
-                'sm:max-w-sm': MODAL_SIZE[0] === size,
-                'sm:max-w-md': MODAL_SIZE[1] === size,
-                'sm:max-w-lg': MODAL_SIZE[2] === size,
-                'sm:max-w-xl': MODAL_SIZE[3] === size,
-                'sm:max-w-2xl': MODAL_SIZE[4] === size,
-                'sm:max-w-3xl': MODAL_SIZE[5] === size,
-                'sm:max-w-4xl': MODAL_SIZE[6] === size,
-                'sm:max-w-5xl': MODAL_SIZE[7] === size,
-                'sm:max-w-6xl': MODAL_SIZE[8] === size,
-                'sm:max-w-full': MODAL_SIZE[9] === size
-              }
-            )}
           >
-            {children}
+            {closeButtonOutside && (
+              <div className="flex pr-1 pt-4">
+                <Button
+                  colors="white"
+                  size="large"
+                  variant="minimal"
+                  onClick={() => {
+                    onCloseWithOutsideButton?.();
+                  }}
+                  aria-label="close icon"
+                >
+                  <XMarkIcon
+                    className="text-base-300 h-6 w-6"
+                    aria-hidden="true"
+                  />
+                </Button>
+              </div>
+            )}
+
+            <div
+              className={twClassNames(
+                `relative flex h-full flex-col overflow-auto bg-white shadow-xl w-screen inset-0`,
+                {
+                  'sm:max-w-sm': MODAL_SIZE[0] === size,
+                  'sm:max-w-md': MODAL_SIZE[1] === size,
+                  'sm:max-w-lg': MODAL_SIZE[2] === size,
+                  'sm:max-w-xl': MODAL_SIZE[3] === size,
+                  'sm:max-w-2xl': MODAL_SIZE[4] === size,
+                  'sm:max-w-3xl': MODAL_SIZE[5] === size,
+                  'sm:max-w-4xl': MODAL_SIZE[6] === size,
+                  'sm:max-w-5xl': MODAL_SIZE[7] === size,
+                  'sm:max-w-6xl': MODAL_SIZE[8] === size,
+                  'sm:max-w-full': MODAL_SIZE[9] === size
+                }
+              )}
+            >
+              {children}
+            </div>
           </div>
-        </div>
-      </Transition.Child>
-    </Transition>
+        </Transition.Child>
+      </Transition>
+    </div>
   );
-};
+});
 
 Slideover.propTypes = {
   children: PropTypes.node,
+  initialFocusRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) })
+  ]),
   onOverlayClick: PropTypes.func,
   onCloseWithOutsideButton: PropTypes.func,
   onEscPress: PropTypes.func,
@@ -140,6 +168,7 @@ Slideover.propTypes = {
 
 Slideover.defaultProps = {
   children: null,
+  initialFocusRef: null,
   onOverlayClick: null,
   onCloseWithOutsideButton: null,
   onEscPress: null,
