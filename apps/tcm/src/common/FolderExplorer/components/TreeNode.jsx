4d6957c349e96e43787b577f1bda5cefe07e9fd7
Pranay Jain <jpranay688@gmail.com>
2023-06-15 12:56:24 +0530

    style: font color, no. of testcases

--- apps/tcm/src/common/FolderExplorer/components/TreeNode.jsx
+++ apps/tcm/src/common/FolderExplorer/components/TreeNode.jsx
@@ -58,7 +58,9 @@ const TreeNode = ({
             {item.name}
           </TMTruncateText>
         }
-        description={`(${item?.cases_count || 0})`}
+        description={
+          <span className="text-base-500">{`(${item?.cases_count || 0})`}</span>
+        }
         hideArrowIcon={!item?.sub_folders_count || false}
         isNodeSelected={selectedNodesId.includes(parseInt(item?.id, 10))}
         onNodeClick={() => onFolderClick(item)}
