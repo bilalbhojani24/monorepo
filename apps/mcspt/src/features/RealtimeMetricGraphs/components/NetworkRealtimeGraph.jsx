c37ecb21d4ee1a776509afcbc7fc5c721cd05080
sanidhya-saraswat <sanidhya.saraswat@gmail.com>
2023-06-16 17:28:13 +0530

    CSPT: Merge live metric graphs

---
+++ apps/mcspt/src/features/RealtimeMetricGraphs/components/NetworkRealtimeGraph.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+import useNetworkRealtimeGraph from '../hooks/useNetworkRealtimeGraph';
+import { formatCurrentRealtimeValue } from '../utils/realtimeGraphUtils';
+
+const NetworkRealtimeGraph = () => {
+  const {
+    networkUploadTimeSeriesData,
+    networkDownloadTimeSeriesData,
+    realtimeNetworkChartOptions
+  } = useNetworkRealtimeGraph();
+
+  return (
+    <div className="mb-8 flex flex-col rounded-lg bg-white p-4 shadow ">
+      <div className="flex">
+        <div className="flex-1">
+          <div className="flex">
+            <div className="text-base-500 text-sm font-medium leading-5">
+              Data Upload
+            </div>
+          </div>
+
+          {networkUploadTimeSeriesData?.length > 0 && (
+            <div className="text-base-900 flex text-3xl font-semibold">
+              {formatCurrentRealtimeValue(networkUploadTimeSeriesData, {
+                unit: 'Kbps'
+              })}
+            </div>
+          )}
+        </div>
+        <div className="flex-1">
+          <div className="flex">
+            <div className="text-base-500 text-sm font-medium leading-5">
+              Data Download
+            </div>
+          </div>
+
+          {networkDownloadTimeSeriesData?.length > 0 && (
+            <div className="text-base-900 flex text-3xl font-semibold">
+              {formatCurrentRealtimeValue(networkDownloadTimeSeriesData, {
+                unit: 'Kbps'
+              })}
+            </div>
+          )}
+        </div>
+      </div>
+
+      <div className="mt-3">
+        <div className="relative h-[182px] flex-1">
+          <div className="absolute left-0 top-0 w-full">
+            {realtimeNetworkChartOptions && (
+              <HighchartsReact
+                highcharts={Highcharts}
+                options={realtimeNetworkChartOptions}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NetworkRealtimeGraph;
