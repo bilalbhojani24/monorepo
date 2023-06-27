32346d8ccf50f9bc070dbcc11b42c74fd8088aeb
Sarosh Malek <sarosh@browserstack.com>
2023-06-27 10:52:59 +0530

    remove console.log

--- apps/accessibility/src/common/IssueItem/NeedsReviewBanner.jsx
+++ apps/accessibility/src/common/IssueItem/NeedsReviewBanner.jsx
@@ -51,7 +51,6 @@ function NodeIssueNavigator({
   };
 
   const getReviewStatusInfo = () => {
-    console.log({ nodeNeedsReviewStatus });
     if (isConfirmedInAllReports === null && !showHiddenIssues) {
       if (showPagination) {
         const modifier = getReviewTagColor(
