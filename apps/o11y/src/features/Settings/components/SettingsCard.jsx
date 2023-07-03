879c167a4384b134fe3b24766a9e4ef28c4a7bd2
Dhruv Rana <22201670+drjaat@users.noreply.github.com>
2023-07-03 15:09:08 +0530

    fix smart tags width

--- apps/o11y/src/features/Settings/components/SettingsCard.jsx
+++ apps/o11y/src/features/Settings/components/SettingsCard.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 
 export default function SettingsCard({ children }) {
   return (
-    <div className="border-base-200 max-h-full flex-1 overflow-auto rounded-lg border bg-white shadow">
+    // eslint-disable-next-line tailwindcss/no-arbitrary-value
+    <div className="border-base-200 max-h-full max-w-[896px] flex-1 overflow-auto rounded-lg border bg-white shadow">
       {children}
     </div>
   );
