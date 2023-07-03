48dd04790ce848a92752172f9665111b17e66c8c
Dhruv Rana <22201670+drjaat@users.noreply.github.com>
2023-07-03 15:15:41 +0530

    removed arbitrary value

--- apps/o11y/src/features/Settings/components/SettingsCard.jsx
+++ apps/o11y/src/features/Settings/components/SettingsCard.jsx
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 
 export default function SettingsCard({ children }) {
   return (
-    // eslint-disable-next-line tailwindcss/no-arbitrary-value
-    <div className="border-base-200 max-h-full max-w-[896px] flex-1 overflow-auto rounded-lg border bg-white shadow">
+    <div className="border-base-200 max-h-full w-full max-w-4xl flex-1 overflow-auto rounded-lg border bg-white shadow">
       {children}
     </div>
   );
