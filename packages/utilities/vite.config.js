5ac36c4ca2a273a5d79706ed1b83446d0578a82a
bilalbhojani24 <bilalbhojani24@gmail.com>
2023-07-20 14:22:36 +0530

    vite config updated

--- packages/utilities/vite.config.js
+++ packages/utilities/vite.config.js
@@ -1,25 +1,19 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
-const { packageViteConfig } = require("@browserstack/vite-config");
+const { packageViteConfig } = require('@browserstack/vite-config');
 
 export default defineConfig(() => ({
   ...packageViteConfig,
   build: {
     lib: {
-      entry: resolve("./index.js"),
-      name: "@bilal/utilities",
-      formats: ["es"],
+      entry: resolve('./index.js'),
+      name: '@bilal/utilities',
+      formats: ['es'],
       fileName: () => `index.js`,
     },
     rollupOptions: {
-      external: ["react", "react-don", "react-router-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-router": "ReactRouter",
-        },
-      },
+      external: ['react', 'react-don', 'react-router-dom'],
     },
     sourcemap: true,
   },
